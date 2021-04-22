8ebe48db338c0f88f9a4008f8415160b368b15b4
unknown <joshkamrass@gmail.com>
2021-04-22 11:01:21 -0400

    converts temperatures to fahrenheit

--- src/reducers/cities-reducer.js
+++ src/reducers/cities-reducer.js
@@ -3,18 +3,21 @@ import { FETCH_FORECAST } from "../actions/index";
 const defaultState = [];
 
 const citiesReducer = (state=defaultState, action) => {
+  // (temperature in Kelvin − 273.15) × 9/5 + 32 = temperature in Fahrenheit
+  const convertKelvinToFahrenheit = (temperatureArr) => temperatureArr.map((t) => Math.round((t - 273.15) * (9/5) + 32)); 
+
   switch (action.type) {
     case FETCH_FORECAST:
       const getWeatherDataFromFetch = (openWeatherData, type) => {
         return openWeatherData.data.list.map((forecastPoint) => forecastPoint.main[type])
-      }
+      };
       const newCity = {
         name: action.payload.data.city.name,
         id: action.payload.data.city.id,
-        temperatures: getWeatherDataFromFetch(action.payload, "temp"),
+        temperatures: convertKelvinToFahrenheit(getWeatherDataFromFetch(action.payload, "temp")),
         pressures: getWeatherDataFromFetch(action.payload, "pressure"),
         humidityPoints: getWeatherDataFromFetch(action.payload, "humidity"),
-      }
+      };
       return [...state, newCity];
   
     default:
