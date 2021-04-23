07d9b05c7497013afaa648a9299e15c8f18854f0
Stefania Spetetchi <steffi@balerion.attlocal.net>
2021-04-23 17:14:14 -0400

    completed format and means

--- src/components/cities-index.js
+++ src/components/cities-index.js
@@ -4,26 +4,31 @@ import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-spark
 import SearchValue from "./search-form.js";
 import _ from 'lodash';
 
+
 const CitiesIndex = () => {
   const cities = useSelector((state) => state.cities);
 
   function renderCities() {
     if (!_.isEmpty(cities)) {
+      let resultTemp = cities.stats.map(t => t.temperature);
+      let resultPressure = cities.stats.map(t => t.pressure);
+      let resultHumidity = cities.stats.map(t => t.humidity);
+
       return (
-        <tr>
-          <td>{cities.name}</td>
-          <td><Sparklines data={cities.stats.map((city => city.temperature))}>
+        <tr >
+          <td style={{ height: '100px' }} className="align-middle"><strong>{cities.name}</strong></td>
+          <td><Sparklines data={cities.stats.map((city => city.temperature))} svgHeight={75}>
             <SparklinesLine color="blue" />
             <SparklinesReferenceLine type="mean" />
-          </Sparklines></td>
-          <td><Sparklines data={cities.stats.map((city => city.pressure))}>
+          </Sparklines><p>{Math.round(_.mean(resultTemp))} F</p></td>
+          <td><Sparklines data={cities.stats.map((city => city.pressure))} svgHeight={75}>
             <SparklinesLine color="green" />
             <SparklinesReferenceLine type="mean" />
-          </Sparklines></td>
-          <td><Sparklines data={cities.stats.map((city => city.humidity))}>
+          </Sparklines><p>{Math.round(_.mean(resultPressure))} hPa</p></td>
+          <td><Sparklines data={cities.stats.map((city => city.humidity))} svgHeight={75}>
             <SparklinesLine color="red" />
             <SparklinesReferenceLine type="mean" />
-          </Sparklines></td>
+          </Sparklines><p>{Math.round(_.mean(resultHumidity))} %</p></td>
         </tr>
       )
     }
@@ -39,13 +44,13 @@ const CitiesIndex = () => {
     <div className="col text-center">
       <SearchValue fetchCities={fetchCities} />
       <hr />
-      <table className="table table-bordered col-md-10 offset-md-1">
+      <table className="table table-hover col-md-12">
         <thead>
           <tr>
             <th style={{ width: '200px' }}>City</th>
-            <th style={{ width: '233px' }}>Temperature</th>
-            <th style={{ width: '233px' }}>Pressure</th>
-            <th style={{ width: '233px' }}>Humidity</th>
+            <th style={{ width: '233px' }}>Temperature (F)</th>
+            <th style={{ width: '233px' }}>Pressure (hPa)</th>
+            <th style={{ width: '233px' }}>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{renderCities()}</tbody>
