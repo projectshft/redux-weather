ca0603ca963929b9d9e071ef4bbe3837a4bc0b57
unknown <joshkamrass@gmail.com>
2021-04-23 10:51:33 -0400

    loads default city data

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,8 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeDefaultCity, fetchForecast } from '../actions';
 import CitySearch from './CitySearch';
 import CityWeatherList from './CityWeatherList';
 import DefaultCityUI from './DefaultCityUI';
 
 const MainPage = () => {
+  const dispatch = useDispatch();
+  const loadDefaultCityInfo = () => {
+    if(localStorage.getItem('defaultCity')) {
+      dispatch(fetchForecast(localStorage.getItem('defaultCity')))
+      dispatch(changeDefaultCity(localStorage.getItem('defaultCity')))
+    }
+  }
+
+  useEffect(() => {
+    loadDefaultCityInfo()
+  }, [])
+
+  
   return (
     <div className="container-fluid text-center">
       <h1>5 Day Forecasts</h1>
