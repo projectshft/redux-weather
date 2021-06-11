b3e663013c2ed9c5d0ff9324fe6e1767fbc4bd36
Jeremy Wheless <whelessjw@gmail.com>
2021-06-11 09:38:46 -0400

    created TableRow component

---
+++ src/components/ForecastRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ForecastRow() {
+  return (
+    <>
+      <tr>
+        <td>Durham</td>
+        <td>52 F</td>
+        <td>1001 hPa</td>
+        <td>63 %</td>
+      </tr>
+    </>
+  );
+}
--- src/components/ForecastTable.js
+++ src/components/ForecastTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ForecastRow from "./ForecastRow";
 
 export default function ForecastTable() {
   return (
@@ -20,24 +21,7 @@ export default function ForecastTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        <ForecastRow />
       </tbody>
     </table>
   );
