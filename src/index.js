899568823c2a3f438ac93a143fcc1c53e6e10507
Jeremy Wheless <whelessjw@gmail.com>
2021-06-11 10:17:39 -0400

    created redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+import { Provider } from "react-redux";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
