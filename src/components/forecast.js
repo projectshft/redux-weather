1bef842720be2e0e4aa8ea9246b0fa69d808e4b6
Daniel Piston <danielpiston@Daniels-MBP.lan>
2021-06-30 20:04:32 -0400

    stuck on pulling temp, pressure, and humidity from state in useSelectors

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -3,7 +3,21 @@ import { useEffect } from "react";
 import { fetchForecast } from "../actions";
 
 const Forecast = () => {
-  const forecast = useSelector((state) => state.posts);
+  const forecastTemps = useSelector((state) => {
+    
+    console.log(state.posts[0])
+
+    // const a = state.posts[0].forEach((i) => state.posts[0][i].main.temp);
+
+    // console.log(a);
+    // return state.posts;
+  });
+
+  // const post = useSelector(({ posts }) => {
+  //   return posts.find((post) => {
+  //     return post.id === parseInt(props.match.params.id);
+  //   });
+  // });
 
   const dispatch = useDispatch();
   
@@ -12,11 +26,22 @@ const Forecast = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchForecast]);
 
-  console.log(forecast);
-  
+  // console.log(forecast[0]);
+  // // want array tempsArr of the 40 temps
+  // // these are forecast[i].main.temp
+  // const makeArr = (element) => {
+  //   tempArr = forecast.map((x) =>
+  //   )
+  // }
+  // want array pressArr of the 40 pressures
+  // these are forecast[i].main.pressure
+
+  // want array humidArr of the 40 humidities
+  // these are forecast[i].main.humidity
+
   return (
     <div>
-      {forecast}
+
     </div>
   );
 };
--- src/reducers/reducer-forecast.js
+++ src/reducers/reducer-forecast.js
@@ -4,8 +4,9 @@ const ForecastReducer = function (state = [], action) {
 
   switch (action.type) {
     case FETCH_FORECAST:
+
       // need to return an array, not object
-      return [action.payload.data, ...state];
+      return [action.payload.data.list, ...state];
     default:
       return state;
   }
