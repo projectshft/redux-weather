c15e8d250a4a603e5a111e1bd9a35704545b7687
Daniel Piston <danielpiston@Daniels-MBP.lan>
2021-07-02 14:09:52 -0400

    added sparklines and header

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -1,13 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchForecast } from "../actions";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine,
+} from "react-sparklines";
+
 
 const Forecast = () => {
   const forecastTemps = useSelector((state) => {
     
     console.log(state.posts[0])
+    
+    // the above is array but when I run any kind of array method on it I get "cant find property of undefined"
 
-    // const a = state.posts[0].forEach((i) => state.posts[0][i].main.temp);
+    // const a = state.posts[0]._____ => state.posts[0][i].main.temp);
 
     // console.log(a);
     // return state.posts;
@@ -38,10 +46,48 @@ const Forecast = () => {
 
   // want array humidArr of the 40 humidities
   // these are forecast[i].main.humidity
+  const city = 'Durham'
+  const tempsArr = [75, 77, 78, 92, 50]
+  const pressArr = [7, 5, 3, 8, 2]
+  const humidArr = [100, 55, 75, 82, 12]
 
   return (
     <div>
+      <div className="container">
 
+        <div className="row">
+          <div className="col-sm" align="center">
+            Durham
+          </div>
+          <div className="col-sm">
+            <Sparklines data={tempsArr}>
+              <SparklinesLine color="#CE93D8" />
+              <SparklinesReferenceLine
+                type="avg"
+                style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+              />
+            </Sparklines>
+          </div>
+          <div className="col-sm">
+            <Sparklines data={pressArr}>
+              <SparklinesLine color="#C5CAE9" />
+              <SparklinesReferenceLine
+                type="avg"
+                style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+              />
+            </Sparklines>
+          </div>
+          <div className="col-sm">
+            <Sparklines data={humidArr}>
+              <SparklinesLine color="#4FC3F7" />
+              <SparklinesReferenceLine
+                type="avg"
+                style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+              />
+            </Sparklines>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,30 @@ const header = (props) => {
     <div>
       <div className="jumbotron text-center">
         <div className="container">
+          <br />
           <h1 className="jumbotron-heading">Redux Weather App</h1>
         </div>
       </div>
+      <br />
+      <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm" align="center">
+            <strong>City</strong>
+          </div>
+          <div className="col-sm" align="center">
+            <strong>Temperature</strong>
+          </div>
+          <div className="col-sm" align="center">
+            <strong>Pressure</strong>
+          </div>
+          <div className="col-sm" align="center">
+            <strong>Humidity</strong>
+          </div>
+        </div>
+        <hr />
+      </div>
+
       <div className="container">{props.children}</div>
     </div>
   );
