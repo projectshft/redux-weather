ad0a1de5e4c2e68531c1e1cf0fd40d14e8c2ef94
Daniel Piston <danielpiston@Daniels-MBP.lan>
2021-07-08 10:53:06 -0400

    added renderForecasts() and it works

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -16,37 +16,6 @@ const Forecast = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetchForecast]);
 
-  // DO SOMETHING LIKE:
-
-  // function renderPosts() {
-  //   if (posts.length > 0) {
-  //     return posts.map((post) => (
-  //       <li className="list-group-item" key={post.id}>
-  //         <Link to={`/posts/${post.id}`}>{post.title}</Link>
-  //       </li>
-  //     ));
-  //   }
-  //   return <div>No posts to show</div>;
-  // }
-
-  function rederForecasts() {
-
-    if (forecast.length > 0) {
-
-      return forecast.map((f) => (
-        // there is f.temps, f.press, f.humid
-        // the averages are what we need
-      ))
-    }
-
-    return <div>Please enter a city</div>
-  }
-  console.log(singleForecast);
-
-  let tempsArr = singleForecast?.temps ? singleForecast.temps : [];
-  let pressArr = singleForecast?.press ? singleForecast.press : [];
-  let humidArr = singleForecast?.humid ? singleForecast.humid : [];
-
   const averageFunc = (array) => {
     let total = array.reduce(function (x, acc) {
       return x + acc;
@@ -54,14 +23,82 @@ const Forecast = () => {
     return Math.round(total / array.length);
   };
 
-  const tempsAvg = averageFunc(tempsArr) + " F";
-  const pressAvg = averageFunc(pressArr) + " hPa";
-  const humidAvg = averageFunc(humidArr) + " %";
-  const cityDisp = singleForecast?.city ? singleForecast.city : '';
+  function renderForecasts() {
+    if (forecast.length > 0) {
+      return forecast.map((f) => (
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm" align="center">
+                {f.city}
+              </div>
+              <div className="col-sm">
+                <Sparklines data={f.temps}>
+                  <SparklinesLine color="#CE93D8" />
+                  <SparklinesReferenceLine
+                    type="avg"
+                    style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+                  />
+                </Sparklines>
+              </div>
+              <div className="col-sm">
+                <Sparklines data={f.press}>
+                  <SparklinesLine color="#C5CAE9" />
+                  <SparklinesReferenceLine
+                    type="avg"
+                    style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+                  />
+                </Sparklines>
+              </div>
+              <div className="col-sm">
+                <Sparklines data={f.humid}>
+                  <SparklinesLine color="#4FC3F7" />
+                  <SparklinesReferenceLine
+                    type="avg"
+                    style={{ stroke: "#546E7A", strokeDasharray: "2, 2" }}
+                  />
+                </Sparklines>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-sm" align="center"></div>
+              <div className="col-sm" align="center">
+                { averageFunc(f.temps)} F
+              </div>
+              <div className="col-sm" align="center">
+                { averageFunc(f.press) } hPa
+              </div>
+              <div className="col-sm" align="center">
+                { averageFunc(f.humid) } %
+              </div>
+            </div>
+            <br />
+          </div>
+        </div>
+      ));
+    }
+
+    return <div>Please enter a city</div>
+  }
+  // console.log(singleForecast);
+
+  // let tempsArr = singleForecast?.temps ? singleForecast.temps : [];
+  // let pressArr = singleForecast?.press ? singleForecast.press : [];
+  // let humidArr = singleForecast?.humid ? singleForecast.humid : [];
+
+  
+
+  // const tempsAvg = averageFunc(tempsArr) + " F";
+  // const pressAvg = averageFunc(pressArr) + " hPa";
+  // const humidAvg = averageFunc(humidArr) + " %";
+  // const cityDisp = singleForecast?.city ? singleForecast.city : '';
 
   return (
     <div>
-       
+     { renderForecasts() }
     </div>
   );
 };
