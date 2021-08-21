6aaa6369b62c85b2b2a324c8120259a592d4a851
Ryan Passer <rpasser93@gmail.com>
2021-08-21 17:10:29 -0400

    Add sum & avg variables for table metrics

--- src/components/city-table.js
+++ src/components/city-table.js
@@ -2,12 +2,20 @@ import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparkli
 
 const CityTable = () => {
   const temperatureData = [5,10,15,20];
+  const temperatureSum = temperatureData.reduce((a, b) => a + b, 0);
+  const temperatureAvg = temperatureSum/temperatureData.length;
+
   const pressureData = [6,13,2,19];
+  const pressureSum = pressureData.reduce((a, b) => a + b, 0);
+  const pressureAvg = pressureSum/pressureData.length;
+
   const humidityData = [1,44,55,39];
+  const humiditySum = humidityData.reduce((a, b) => a + b, 0);
+  const humidityAvg = humiditySum/humidityData.length;
 
   return (
     <table className="table">
-      <thead className="table-light">
+      <thead className="table-light text-center">
         <tr>
           <th scope="col">City</th>
           <th scope="col">Temperature (F)</th>
@@ -24,18 +32,21 @@ const CityTable = () => {
             <SparklinesLine color="orange" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
+          <div className="text-center">{temperatureAvg} F</div>
           </td>
           <td>
           <Sparklines data={pressureData}>
             <SparklinesLine color="green" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
+          <div className="text-center">{pressureAvg} hPa</div>
           </td>
           <td>
           <Sparklines data={humidityData}>
             <SparklinesLine color="purple" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
+          <div className="text-center">{humidityAvg} %</div>
           </td>
         </tr>
       </tbody>
