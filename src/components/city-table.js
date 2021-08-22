33034227ec8c909125167d01969ebfb335da16d3
Ryan Passer <rpasser93@gmail.com>
2021-08-22 03:41:55 -0400

    Display average weather data from API under charts

--- src/components/city-table.js
+++ src/components/city-table.js
@@ -1,17 +1,11 @@
 import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
 
 const CityTable = (props) => {
-  // const temperatureData = [5,10,15,20];
-  // const temperatureSum = temperatureData.reduce((a, b) => a + b, 0);
-  // const temperatureAvg = temperatureSum/temperatureData.length;
-
-  // const pressureData = [6,13,2,19];
-  // const pressureSum = pressureData.reduce((a, b) => a + b, 0);
-  // const pressureAvg = pressureSum/pressureData.length;
-
-  // const humidityData = [1,44,55,39];
-  // const humiditySum = humidityData.reduce((a, b) => a + b, 0);
-  // const humidityAvg = humiditySum/humidityData.length;
+ 
+  const dataAvg = (array) => {
+    const total = array.reduce((a, b) => a + b, 0);
+    return Math.round(total/array.length);
+  }
 
   return props.searches.map((search, i) => (
   
@@ -26,21 +20,21 @@ const CityTable = (props) => {
             <SparklinesLine color="orange" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
-          <div className="text-center">xxx F</div>
+          <div className="text-center">{dataAvg(search.data.list.map((block)=>{return block.main.temp;}))} {'\xB0F'}</div>
           </td>
           <td>
           <Sparklines data={search.data.list.map((block)=>{return block.main.pressure;})}>
             <SparklinesLine color="green" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
-          <div className="text-center">xxx hPa</div>
+          <div className="text-center">{dataAvg(search.data.list.map((block)=>{return block.main.pressure;}))} hPa</div>
           </td>
           <td>
           <Sparklines data={search.data.list.map((block)=>{return block.main.humidity;})}>
             <SparklinesLine color="purple" />
             <SparklinesReferenceLine type="avg" />
           </Sparklines>
-          <div className="text-center">xxx %</div>
+          <div className="text-center">{dataAvg(search.data.list.map((block)=>{return block.main.humidity;}))}%</div>
           </td>
         </tr>
       </tbody>
