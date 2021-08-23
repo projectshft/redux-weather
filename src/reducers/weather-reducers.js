3b3ec16c0c348ece36f83dd563764c87b3702650
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-08-23 00:22:01 -0400

    Reducer update

--- src/reducers/weather-reducers.js
+++ src/reducers/weather-reducers.js
@@ -3,7 +3,14 @@ import { FETCH_WEATHER } from "../actions";
 const weatherReducer = function (state = {}, action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return action.payload.data
+      return action.payload.data.map (function (city) {
+        return {
+          city: city.dt,
+          temperature: city.main.temp,
+          pressure: city.main.pressure,
+          humidity: city.main.humidity
+        };
+      });
     default:
       return state
   }
