9312f369a4fcf06ed9cd2a035d94730faeb4b80e
timothybulla <83347577+timothybulla@users.noreply.github.com>
2021-08-22 22:58:56 -0400

    Search Component

--- src/components/weather-index.js
+++
@@ -1,6 +0,0 @@
-// import { Link } from "react-router-dom";
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
-// import { fetchPosts } from "../actions";
-// import { useEffect } from "react";
-
