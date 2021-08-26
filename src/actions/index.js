3c7ae8d13038f680fe39c24c8f36980f09f7d839
Philip Krynicki <philipkrynicki@Philips-MacBook-Air.local>
2021-08-26 11:18:29 -0500

    Updated index js under actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,38 @@
-import { bindActionCreators } from "redux"
-
-export const addTemperature = (temperature) => {
+export const addCityName = (value) => {
   return {
-    type: 'TEMPERATURE/ADD_TEMPERATURE',
-    temperature: temperature,
+    type: 'add-city-name',
+    payload: {
+      value
+    }
+  }
+}
+
+export const addPressure = (city, value) => {
+  return {
+    type: 'add-pressure',
+    payload: {
+      city,
+      value
+    }
+  }
+}
+
+export const addHumidity = (city, value) => {
+  return {
+    type: 'add-humidity',
+    payload: {
+      city,
+      value
+    }
+  }
+}
+
+export const addTemperature = (city, value) => {
+  return {
+    type: 'add-temperature',
+    payload: {
+      city,
+      value
+    }
   }
 }
