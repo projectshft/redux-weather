d9df5a3fde308f5f61d358ba5a26914ede192ea5
Jason Stover <jstover8807@gmail.com>
2021-11-08 12:51:26 -0500

    useEffect issue, state object updated only after 3 searches, map function not rendering

--- src/components/cities-index.js
+++ src/components/cities-index.js
@@ -8,62 +8,73 @@ import { fetchCity } from '../actions';
 import { render } from '@testing-library/react';
 
 
-const CitiesIndex = (props) => {   
+const CitiesIndex = () => {   
+    
     const defaultCity = "Portland";
     
     useEffect(() => {
         dispatch(fetchCity(defaultCity));
 
     }, []);    
+      
 
     const [city, setCity] = useState("");
     const [temperatures, setTemps] = useState([]);
     const [pressures, setPressures] = useState([]);
     const [humidities, setHumidity] = useState([]);
+    //create state variable for the collection of searched cities
+    const [cityCollection, setCityCollection] = useState([{
+        searchedCities: [{}]
+    }]);
     const dispatch = useDispatch();    
 
-    const cities = useSelector(state => state.cities)   
+    const cities = useSelector(state => state.cities)   //Names payload.data cities
         
     const handleFormSubmit = (e) => {
         e.preventDefault();
     };
     
-    function handleButtonClick (data) { 
+    function handleButtonClick (data) {     
         dispatch(fetchCity(data));                        
     };
     
-    function getData () {
-        for (let i = 0; i < cities.length; i++) {
-            console.log(cities[i]);
+    function getData () {  //iterates through api call (5 total) and creates arrays of needed values
+        for (let i = 0; i < cities.length; i++) {            
             setTemps(temperatures => [...temperatures, cities[i].main.temp])
             setPressures(pressures => [...pressures, cities[i].main.pressure])
             setHumidity(humidities => [...humidities, cities[i].main.humidity])
-        }        
+        }
+        
+        const searchedCity = {temps: temperatures, pressure: pressures, humidity: humidities};  //Stores needed values in state array of objects
+        setCityCollection(cityCollection => [...cityCollection, {searchedCity}])  //begins to work after the third button click.  Why?
+        console.log(cityCollection);
+        
         
     }
     
     function renderCities() {             
         if (!_.isEmpty(cities)) {         
-            return  (                
-                <tr className="city-info-row" >
+            return  cityCollection.map((row) => {              //map/iterate through array of objects rendering them to the DOM
+                <tr className="city-info-row" key={row}>                    
                     <td className="city-name">{city}</td>                    
-                        <Sparklines data={temperatures}>
+                        <Sparklines data={row.temps}>
                             <SparklinesLine />
                             <SparklinesReferenceLine type="mean" />
                         </Sparklines>
                         
-                        <Sparklines data={pressures}>
+                        <Sparklines data={row.pressure}>
                             <SparklinesLine />
                             <SparklinesReferenceLine type="mean" />
                         </Sparklines>
                     
-                        <Sparklines data={humidities}>
+                        <Sparklines data={row.humidity}>
                             <SparklinesLine />
                             <SparklinesReferenceLine type="mean" />
                         </Sparklines>                   
                 </tr>
-            )
-        }        
+            
+        })
+    }        
          return <div>No cities have been selected</div>        
 }   
     
