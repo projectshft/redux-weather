75120c7b2c78a3517e01ff6f654bf2d66686c03e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-01-09 16:32:27 -0500

    successfully got city mockState to post

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+
 const Show = () => {
+  const city = useSelector(state => state.city)
+  
   return (
     <div>
-      <p>City:</p>
+      <p>City: {city}</p>
     </div>
   )
 }
