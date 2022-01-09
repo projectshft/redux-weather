c4067071265fc63390b111be733cc0989eef2424
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-01-09 16:18:24 -0500

    set up a reducer with adefault value

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,5 +1,14 @@
-const Reducer = () => {
 
+
+export const inputClick = {
+  type: 'INPUT_CLICK'
+}
+
+const Reducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
 
 export default Reducer
