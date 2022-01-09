26a0037f25175ee02a239423aa180df52e8b3161
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-01-09 16:41:19 -0500

    basic column headings set up

--- src/components/Show.js
+++ src/components/Show.js
@@ -7,10 +7,13 @@ const Show = () => {
     <div>
       <div className="container">
         <div className="row">
-
+          <div className="col-3">City: {city}</div>
+          <div className="col-3">Temperature (F)</div>
+          <div className="col-3">Pressure (hPa)</div>
+          <div className="col-3">Humidity (%)</div>
         </div>
       </div>
-      <p>City: {city}</p>
+      
     </div>
   )
 }
