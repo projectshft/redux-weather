e99cb1f7fb2bf198689f8d0568d46dce787d6946
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-01-18 05:54:59 -0500

    I have open weather map api connected to a root url

--- src/Actions.js
+++ src/Actions.js
@@ -1,3 +1,8 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9b71dd7687d5daeb5225c83041aa3ed4';
+
+
 export const searchTermClick = (searchTerm) => {
   return {
     type: 'SEARCHTERM_CLICK', 
@@ -6,7 +11,7 @@ export const searchTermClick = (searchTerm) => {
 }
 
 export const fetchPosts = () => {
-  const request = 
+  const request = axios.get(ROOT_URL);
   
   return {
     type: 'FETCH_POSTS',
