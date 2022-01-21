a3367702aacee5442515d099308bcabe26be41d7
j-d-hunt <jdhunt@Jordans-MBP.lan>
2022-01-21 14:42:59 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -53,71 +53,79 @@ function App() {
   }
 
   return (
-    <div className="container-fluid">
-      <div className="row" >
-        <div className="col-md-6 offset-md-3">
-          <div className="App">
-            <h1>Redux Weather</h1>
-            <form>
-              <div className="form-group">
-              <input 
-                type="text" 
-                value= {cityInput}
-                onChange={e => setCityInput(e.target.value)}
-                className="form-control" 
-                placeholder="Get a five-day forecast in your favorite cities" />
-              </div>
-            </form>
-          </div>
+    <div className="container">           
+      
+      <div className="row" name="header" >
+        <div className="col"></div>
+        <div className="col"><h1>Redux Weather</h1></div>
+        <div className="col"></div>
+      </div> 
+      
+      <div className="row" name="input">
+      <div className="col"></div>
+      <div className="col">
+          <input 
+            type="text" 
+            value= {cityInput}
+            onChange={e => setCityInput(e.target.value)}
+            className="" 
+            placeholder="Search for your city...">
+          </input>  
+          <button 
+            type="button" 
+            onClick={onClick} 
+            className="btn btn-primary">Search
+          </button>
         </div>
-        <div className="col">
-          <button type="button" onClick={onClick} className="btn btn-primary">Search</button>
-        </div>
-        <div className="row">
-          <table >
-            <thead>
-              <tr>
-                <th scope="col" style={{'textAlign':'center'}}><b>City</b></th>
-                <th scope="col" style={{'textAlign':'center'}}><b>Temperature (F)</b></th>
-                <th scope="col" style={{'textAlign':'center'}}><b>Pressure (hPa)</b></th>
-                <th scope="col" style={{'textAlign':'center'}}><b>Humidity (%)</b></th>
-              </tr>
-            </thead>
-            <tbody>
-              {cities.map(city => (
-                <tr key={city.name}>
-                  <td>
-                    <center><b>{city.city.name}</b></center>
-                  </td>
-                  <td>
-                    <Sparklines data={getTemps(city.list)}>
-                      <SparklinesLine color="blue" style={{ fill: "7DA7F5" }}/>
-                      <SparklinesReferenceLine type="mean" />
-                    </Sparklines>
-                    <center><b>{calculateMean(getTemps(city.list))}(F)</b></center>
-                  </td>
-                  <td>
-                    <Sparklines data={getPressure(city.list)}>
-                      <SparklinesLine color="orange" style={{ fill: "E6B36F" }}/>
-                      <SparklinesReferenceLine type="mean" />
-                    </Sparklines>
-                    <center><b>{calculateMean(getPressure(city.list))}hPa</b></center>
-                  </td>
-                  <td>
-                    <Sparklines data={getHumidity(city.list)}>
-                      <SparklinesLine color="green" style={{ fill: "B9F0A3" }}/>
-                      <SparklinesReferenceLine type="mean" />
-                    </Sparklines>
-                    <center><b>{calculateMean(getHumidity(city.list))}%</b></center>
-                  </td>
-                </tr>
-                ))
-              }
-            </tbody>
-          </table>
-        </div>
-      </div>
+        <div className="col"></div>
+      </div>    
+             
+    
+    {/*Data Table*/}
+    <div className="row">
+      <table style={{ "width" : "80%", "align" : "center" }}>
+        <thead>
+          <tr>
+            <th scope="col" style={{'textAlign':'center'}}><b>City</b></th>
+            <th scope="col" style={{'textAlign':'center'}}><b>Temperature (F)</b></th>
+            <th scope="col" style={{'textAlign':'center'}}><b>Pressure (hPa)</b></th>
+            <th scope="col" style={{'textAlign':'center'}}><b>Humidity (%)</b></th>
+          </tr>
+        </thead>
+        <tbody>
+          {cities.map(city => (
+            <tr key={city.name}>
+              <td>
+                <center><b>{city.city.name}</b></center>
+              </td>
+              <td>
+                <Sparklines data={getTemps(city.list)}>
+                  <SparklinesLine color="blue" style={{ fill: "7DA7F5" }}/>
+                  <SparklinesReferenceLine type="mean" />
+                </Sparklines>
+                <center><b>{calculateMean(getTemps(city.list))}(F)</b></center>
+              </td>
+              <td>
+                <Sparklines data={getPressure(city.list)}>
+                  <SparklinesLine color="orange" style={{ fill: "E6B36F" }}/>
+                  <SparklinesReferenceLine type="mean" />
+                </Sparklines>
+                <center><b>{calculateMean(getPressure(city.list))}hPa</b></center>
+              </td>
+              <td>
+                <Sparklines data={getHumidity(city.list)}>
+                  <SparklinesLine color="green" style={{ fill: "B9F0A3" }}/>
+                  <SparklinesReferenceLine type="mean" />
+                </Sparklines>
+                <center><b>{calculateMean(getHumidity(city.list))}%</b></center>
+              </td>
+            </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
+  </div>   
   );
 }
 
