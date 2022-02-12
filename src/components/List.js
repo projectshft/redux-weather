644b1d9ed3194275036464a20b19e24ad6b7841b
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-02-12 11:48:49 -0500

    List populates!

--- src/components/List.js
+++ src/components/List.js
@@ -4,10 +4,10 @@ const List = () => {
 
 const cityData = useSelector(state => state)
 
-const city = [
-  {id: 0, name: 'Dover', pressure: '3', temperature: '7', humidity: '9'},
-  {id: 1, name: 'Chicago', pressure: '13', temperature: '17', humidity: '19'}
-];
+// const city = [
+//   {id: 0, name: 'Dover', pressure: '3', temperature: '7', humidity: '9'},
+//   {id: 1, name: 'Chicago', pressure: '13', temperature: '17', humidity: '19'}
+// ];
 
 const renderList = () => {
   if (!cityData) {
@@ -19,9 +19,9 @@ const renderList = () => {
       <ul>{cityData.map((city, index) => <li key={index}> 
                                             <div className="row">
                                               <div className="col-3">{city.name}</div>
-                                              <div className="col-3">{city.temperature}</div>
-                                              <div className="col-3">{city.pressure}</div>
-                                              <div className="col-3">{city.humidity}</div>
+                                              <div className="col-3">{city.main.temp}</div>
+                                              <div className="col-3">{city.main.pressure}</div>
+                                              <div className="col-3">{city.main.humidity}</div>
                                             </div>
                                           </li>)}
       </ul>
