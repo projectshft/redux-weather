d15a220e152c5be4dd4136c4ec76a1caaa51faaa
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-02-25 11:06:38 -0600

    index.js routing and imports complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import ChartsIndex from "./components/posts-index";
+import Header from "./components/header";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Header>
+        <Switch>
+          <Route path='/' component={ChartsIndex} />
+        </Switch>
+      </Header>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
