f41e50298a12300dbc2b8abcced1d7cba100b0d1
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-04-24 15:06:18 -0500

    messing around with sparklines

--- src/components/weather-table.js
+++ src/components/weather-table.js
@@ -1,19 +1,60 @@
-import { fetchCities } from "../actions";
-
+// import _ from 'lodash';
+import { useSelector } from "react-redux";
+import {
+  Sparklines, SparklinesLine, SparklinesReferenceLine
+} from "react-sparklines";
 
 const Weather = () => {
-  const handleClick = () => {
-    console.log('hi')
-    fetchCities();
-  }
-  
+  const cities = useSelector((state) => state.cities);
+
+  const renderCities = () => {
+    return cities.map((city) => {
+      const tempData = city.temps;
+      // const pressureData = city.pressure;
+      // const humidityData = city.humidity;
+      return (
+        <tr key={city.cityName}>
+          <th scope="row">{city.cityName}</th>
+          <td>
+            <Sparklines data={tempData}>
+              <SparklinesLine />
+              <SparklinesReferenceLine type="mean" />
+            </Sparklines>
+            <h2>
+              {city.averageTemp}°
+            </h2>
+          </td>
+          {/* <td>
+            <Sparklines data={pressureData}>
+              <SparklinesLine />
+              <SparklinesReferenceLine type="mean" />
+            </Sparklines></td>
+          <td>
+          <Sparklines data={humidityData}>
+              <SparklinesLine />
+              <SparklinesReferenceLine type="mean" />
+            </Sparklines>
+          </td> */}
+        </tr>
+      );
+    });
+  };
+
   return (
-    <div>
-      <button
-        onClick={handleClick}
-      >Hi</button>
+    <div className="container text-center">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">City</th>
+            <th scope="col">Temperature (F)</th>
+            <th scope="col">Pressure (hPa)</th>
+            <th scope="col">Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>{renderCities()}</tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 export default Weather;
--- src/reducers/reducer-cities.js
+++ src/reducers/reducer-cities.js
@@ -1,15 +1,45 @@
 import { FETCH_CITIES } from '../actions';
 
-const defaultState = {
-  cities: {
-    Chetek: {
-
-    },
-    Moscow: {
-
-    }
+const defaultState = [
+  {
+    cityName: 'Chetek',
+    temps: [
+      54,
+      53,
+      59,
+      60,
+      62,
+      58,
+      40
+    ],
+    pressure: [
+      44,
+      43,
+      22,
+      65,
+      34,
+      37,
+      30
+    ],
+    humidity: [
+      5,
+      3,
+      6,
+      20,
+      34,
+      40,
+      32
+    ],
+    averageTemp: 50
+  },
+  {
+    cityName: 'Moscow',
+    temps: [43,43,43,4,323,243,543],
+    pressure: 39,
+    humidity: 20,
+    averageTemp: 89
   }
-}
+]
 
 const CitiesReducer = function(state = defaultState, action) {
   switch (action.type) {
