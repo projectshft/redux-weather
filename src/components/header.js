50e50b67e81b3b3de53b8e595ee94ce10bf39edc
Emily Freeman <freeman.ecf@gmail.com>
2022-04-25 21:48:15 -0400

    all working

--- src/components/header.js
+++ src/components/header.js
@@ -53,9 +53,9 @@ const Header = function () {
         return (
              <tr key={index}>
                <td>{item.city}</td>
-               <td><Sparklines data={item.temp}><SparklinesLine color="blue" /></Sparklines></td>
-               <td><Sparklines data={item.pressure}><SparklinesLine color="red" /></Sparklines></td>
-               <td><Sparklines data={item.humidity}><SparklinesLine color="green" /></Sparklines></td>
+               <td><Sparklines data={item.temp}><SparklinesLine color="blue" /></Sparklines>{Math.round((item.temp[0] - 273) * 1.8 + 32)}</td>
+               <td><Sparklines data={item.pressure}><SparklinesLine color="red" /></Sparklines>{item.pressure[0]}</td>
+               <td><Sparklines data={item.humidity}><SparklinesLine color="green" /></Sparklines>{item.humidity[0]}</td>
            </tr>
          )
       })  
@@ -71,14 +71,14 @@ const Header = function () {
          </div>
         </div>
       </form>
-      
-      <table className="table">
-  <thead className="thead-dark">
+
+      <table className="table" >
+  <thead >
     <tr>
-      <th scope="col">City</th>
-      <th scope="col">Temperature</th>
-      <th scope="col">Pressure</th>
-      <th scope="col">Humidity</th>
+      <th>City</th>
+      <th>Temperature</th>
+      <th>Pressure</th>
+      <th>Humidity</th>
     </tr>
   </thead>
     <tbody>{renderForecasts()}</tbody>
