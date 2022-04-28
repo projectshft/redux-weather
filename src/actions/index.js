fd2a58d2b8244a1f322e977452f635ddee687608
Alex Rodgers <skyewulff@outlook.com>
2022-04-27 21:46:58 -0400

    build action for fetching data for a new city

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 export const FETCH_CITY = "FETCH_CITY";
 
+const API_KEY = 'eef46e88eea99fdfcbf3e442af90b863';
+
 export const fetchCity = () => {
-    const request = axios.get();
+    const cityWeather = axios.get(`api.openweathermap.org/data/2.5/forecast?q=tuscon&appid=${API_KEY}`);
 
     return {
         type: FETCH_CITY,
-        payload: request
+        payload: cityWeather
     }
 }
