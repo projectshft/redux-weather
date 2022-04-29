4002d1f26d6ea026ee5948aca461863c7ecabb9d
Alex Rodgers <skyewulff@outlook.com>
2022-04-28 23:10:14 -0400

    add Sparklines for temp/pressure/humidity in City component

--- src/components/City.js
+++ src/components/City.js
@@ -1,6 +1,29 @@
+import { SparklinesLine } from "react-sparklines";
+import { SparklinesReferenceLine } from "react-sparklines";
+import { Sparklines } from "react-sparklines";
+
 const City = (props) => {
     return (
-        <div>City data will go here</div>
+        <div className="row">
+            <div className="col">
+                <Sparklines data={props.weather.map(item => item.main.temp)}>
+                    <SparklinesLine />
+                    <SparklinesReferenceLine />
+                </Sparklines>
+            </div>
+            <div className="col">
+                <Sparklines data={props.weather.map(item => item.main.pressure)}>
+                    <SparklinesLine />
+                    <SparklinesReferenceLine />
+                </Sparklines>
+            </div>
+            <div className="col">
+                <Sparklines data={props.weather.map(item => item.main.humidity)}>
+                    <SparklinesLine />
+                    <SparklinesReferenceLine />
+                </Sparklines>
+            </div>
+        </div>
     )
 }
 
