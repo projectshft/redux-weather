95544b1270d7c28bc4a8e8e1eace78cad0aaf57e
Alex Rodgers <skyewulff@outlook.com>
2022-04-28 22:40:21 -0400

    create function in Cities component that will handle rendering logic; subscribe to cities slice of store in same

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,9 +1,25 @@
+import { useSelector } from 'react-redux';
 import City from './City';
 
+const renderCities = arr => {
+    // debugger;
+    // console.dir(arr);
+    // debugger;
+    if(arr.length < 1) {
+        console.log('there are no cities yet');
+        return <span>No city data yet (this is just a placeholder)</span>
+    };
+    return arr.map(city => {
+        return <span>{city.name}</span>
+    })
+}
+
 const Cities = (props) => {
+    const cities = useSelector(state => state.cities.cities);
+    // console.dir(cities);
     return (
         <div>
-            <City />
+            {renderCities(cities)}
         </div>
     )
 }
