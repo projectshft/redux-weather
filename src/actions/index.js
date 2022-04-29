a7b1e8e051091d2910994b1fb1877b7cdd6f5d95
Alex Rodgers <skyewulff@outlook.com>
2022-04-28 23:17:56 -0400

    change dispatching logic in SearchForm to take city name bound to input; update URL in action to use it

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,9 @@ export const FETCH_CITY = "FETCH_CITY";
 
 const API_KEY = 'eef46e88eea99fdfcbf3e442af90b863';
 
-export const fetchCity = () => {
+export const fetchCity = (city) => {
     // console.log('fetching city');
-    const cityWeather = axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=tucson&appid=${API_KEY}`, {baseURL: ''});
+    const cityWeather = axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`, {baseURL: ''});
 
     return {
         type: FETCH_CITY,
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,29 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchCity } from "../actions";
 
 const SearchForm = () => {
+    const [cityInput, setCityInput] = useState('');
+
     const dispatch = useDispatch();
+
+    const handleInputChange = (evt) => {
+        setCityInput(evt.target.value);
+    }
 
     const handleSubmit = evt => {
         evt.preventDefault();
         dispatch(
-            fetchCity()
+            fetchCity(cityInput)
         );
+        setCityInput('');
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="col col-11">
-                    <input type="text" name="text" placeholder="Get a five-day forecast in your favorite cities"/>
+                    <input type="text" name="text" value={cityInput} onChange={handleInputChange} placeholder="Get a five-day forecast in your favorite cities"/>
                 </div>
                 <div className="col col-1">
                     <button type="submit" className="btn btn-secondary btn-sm">Submit</button>
