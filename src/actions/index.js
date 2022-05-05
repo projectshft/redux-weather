28e97d0f241654bf2da6882cb58113855031b5a7
Alex Rodgers <skyewulff@outlook.com>
2022-05-04 22:28:49 -0400

    return null in error handler of city GET request

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const API_KEY = 'eef46e88eea99fdfcbf3e442af90b863';
 
 export const fetchCity = (city) => {
     const cityWeather = axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`, {baseURL: ''}).catch(error => {
-        //possibly only return something if no error?
+        return null;
     });
 
     return {
