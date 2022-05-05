5e210bcb22c92763a39be6dce1ba00717ed81350
Alex Rodgers <skyewulff@outlook.com>
2022-05-04 22:25:15 -0400

    create function in City component to create data arrays for graphs to move the logic out of the markup

--- src/components/City.js
+++ src/components/City.js
@@ -9,25 +9,38 @@ const City = (props) => {
         return cities.entries[props.id]
     });
 
+    const createSparklinesDataArray = dataType => {
+        switch (dataType) {
+            case 'temperature':
+                return cityData.weather.map(item => (1.8 * (item.temp - 273) + 32));
+            case 'pressure':
+                return cityData.weather.map(item => item.pressure);
+            case 'humidity':
+                return  cityData.weather.map(item => item.humidity);
+            default:
+                return;
+        }
+    }
+
     return (
         <tr className="City">
             <td>{cityData.name}</td>
             <td className="col">
-                <Sparklines data={cityData.weather.map(item => (1.8 * (item.temp - 273) + 32))} height={125}>
+                <Sparklines data={createSparklinesDataArray('temperature')} height={125}>
                     <SparklinesLine style={{stroke:"#ffaf63", fill:"#ffaf63", fillOpacity:".125"}}/>
                     <SparklinesReferenceLine />
                 </Sparklines>
                 <p>{`${cityData.averages.avgTemp}\u00b0 F`}</p>
             </td>
             <td className="col">
-                <Sparklines data={cityData.weather.map(item => item.pressure)} height={125}>
+                <Sparklines data={createSparklinesDataArray('pressure')} height={125}>
                     <SparklinesLine style={{stroke:"#00800b", fill:"#00800b", fillOpacity:".125"}}/>
                     <SparklinesReferenceLine />
                 </Sparklines>
                 <p>{`${cityData.averages.avgPress} hPa`}</p>
             </td>
             <td className="col">
-                <Sparklines data={cityData.weather.map(item => item.humidity)} height={125}>
+                <Sparklines data={createSparklinesDataArray('humidity')} height={125}>
                     <SparklinesLine style={{stroke:"#2b2b2b", fill:"#2b2b2b", fillOpacity:".125"}}/>
                     <SparklinesReferenceLine />
                 </Sparklines>
