40cf7d5c476e18d9825f600a530bc639f84c2880
Alex Rodgers <skyewulff@outlook.com>
2022-05-05 22:59:01 -0400

    tweak bootstrap styling of searchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -24,10 +24,10 @@ const SearchForm = () => {
         <form onSubmit={handleSubmit} className="SearchForm">
             <div className="row">
                 <div className="col col-11">
-                    <input type="text" name="text" value={cityInput} onChange={handleInputChange} placeholder="Get a five-day forecast in your favorite cities"/>
+                    <input className="form-control" type="text" name="text" value={cityInput} onChange={handleInputChange} placeholder="Get a five-day forecast in your favorite cities"/>
                 </div>
                 <div className="col col-1">
-                    <button type="submit" className="btn btn-secondary btn-sm">Submit</button>
+                    <button type="submit" className="btn btn-secondary btn-md">Submit</button>
                 </div>
             </div>
         </form>
