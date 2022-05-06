b1ea6b61499d7cd07f494da3cd0aae094536c355
Alex Rodgers <skyewulff@outlook.com>
2022-05-05 23:27:32 -0400

    add logic to prevent numbers being used in city search input and to trim any spaces off city name submission

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -12,10 +12,18 @@ const SearchForm = () => {
         setCityInput(evt.target.value);
     }
 
+    //prevent cities from being able to be retrieved using numbers, which the api does allow but would not be reasonable user interface
+    const handleKeyDown = evt => {
+        let keyCode = evt.keyCode;
+        if (keyCode >= 48 && keyCode <= 57) {
+            evt.preventDefault();
+        }
+    }
+
     const handleSubmit = evt => {
         evt.preventDefault();
         dispatch(
-            fetchCity(cityInput)
+            fetchCity(cityInput.trim())
         );
         setCityInput('');
     }
@@ -24,7 +32,7 @@ const SearchForm = () => {
         <form onSubmit={handleSubmit} className="SearchForm">
             <div className="row">
                 <div className="col col-11">
-                    <input className="form-control" type="text" name="text" value={cityInput} onChange={handleInputChange} placeholder="Get a five-day forecast in your favorite cities"/>
+                    <input className="form-control" type="text" name="text" value={cityInput} onChange={handleInputChange} onKeyDown={handleKeyDown} placeholder="Get a five-day forecast in your favorite cities" />
                 </div>
                 <div className="col col-1">
                     <button type="submit" className="btn btn-secondary btn-md">Submit</button>
