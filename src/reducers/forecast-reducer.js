feb1909dc0a0629b0628359d8d1a0edd5d0f8bb6
RyanCaimano <ryancaimano@gmail.com>
2022-09-09 20:12:48 -0500

    ForecastReducer now includes map method. No errors upon returning data for any given city (in the console, that is). However, something doesn't feel right about my second return block. Unable to detect its degree of functionality in the dev-tools. 13 hours in and cannot focus clearly, so waiting for tomorrow morning's mentor meeting to get clarification on the object I am trying to return.

--- src/reducers/forecast-reducer.js
+++ src/reducers/forecast-reducer.js
@@ -3,10 +3,16 @@ import { FETCH_FORECAST } from "../actions";
 const ForecastReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_FORECAST:
-      return action.payload;
+      return action.payload.data.list.map(function (f) {
+        return {
+          temperature: f.main.temp, 
+          pressure: f.main.pressure, 
+          humidity: f.main.humidity 
+          }
+      });
     default:
       return state;
   }
 };
 
-export default ForecastReducer;
+export default ForecastReducer; 
