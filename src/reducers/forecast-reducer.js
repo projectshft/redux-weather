6d20c3098c055f686475de1f6dc6e1995de7287d
RyanCaimano <ryancaimano@gmail.com>
2022-09-10 13:53:59 -0500

    Modified fetchForecast function in actions. Rewrote code for forecast-reducer just so I can see the result of the mapping of the data. Next up is stubbing out cities-show.

--- src/reducers/forecast-reducer.js
+++ src/reducers/forecast-reducer.js
@@ -3,13 +3,16 @@ import { FETCH_FORECAST } from "../actions";
 const ForecastReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_FORECAST:
-      return action.payload.data.list.map(function (f) {
+      
+    let newState = action.payload.data.list.map(function (f) {
         return {
           temperature: f.main.temp, 
           pressure: f.main.pressure, 
           humidity: f.main.humidity 
           }
       });
+      console.log(newState);
+      return newState;
     default:
       return state;
   }
