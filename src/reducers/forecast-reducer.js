44a84fac7f563b461e858d1cea19aea14509ecde
RyanCaimano <ryancaimano@gmail.com>
2022-09-11 21:21:59 -0500

    State is being updated correctly. Additional cities won't render on page because the table data rows do not have a key. Next up is keys to the td rows.

--- src/reducers/forecast-reducer.js
+++ src/reducers/forecast-reducer.js
@@ -4,14 +4,18 @@ const ForecastReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_FORECAST:
       
-    return action.payload.data.list.map(function (f) {
+    let newCity = [...action.payload.data.list.map(function (f) {
         return {
           city: action.payload.data.city.name,
           temperature: f.main.temp, 
           pressure: f.main.pressure, 
           humidity: f.main.humidity 
           };
-      });
+      }),...state];
+
+      
+      return newCity;
+
       
     default:
       return state;
