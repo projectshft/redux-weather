f7443522a6f9f2a4af758882fae328a47e70ccea
RyanCaimano <ryancaimano@gmail.com>
2022-09-14 12:07:10 -0500

    Post-mentor meeting. Tremendous insights from Ben, especially regarding the use of variables to be injected as jsx in my returns and even outside of my returns. Also got options for how to filter my data such that it returns data for a given city..focused on self-contained array helper methods. Chose not to handle each array by using object constructor with assign nor entries because the latter is unfamiliar territory atm. Currently the page renders multiple rows with appropriate data unique to city, however the city names are being duplicated with additional city searches. Next up is setting up breakpoints so I can dive into the console and test out how my filter method is working and how it is evaluating what is true/false.

--- src/components/cities-show.js
+++ src/components/cities-show.js
@@ -1,167 +1,112 @@
 import { useSelector } from "react-redux";
-// import React from "react";
-// import  {Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import React from "react";
+import  {Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 const CitiesShow = () => {
 
   const forecasts = useSelector((state) => state.forecasts);
+ 
+  
+  
+  
 
-  forecasts.map ((forecast) => {
-    
+  
 
-    
-  });
-
-  const eachForecast = [{
-    city: [],
-    temp: [],
-    pressure: [],
-    humidity: []
-  }];
+  //map each forecast to its own array that holds key-value pairs
+  // forecasts.map ((forecast) => {
+  //   let forecastArray = Object.entries(forecast); 
+  //   console.log(forecastArray);
+  
+  //   return forecastArray;
+  // });
    
 
 
-  // const eachForecast = [];
-
-  // const mapForecast = forecasts.map((forecast) => {
-    
-    
-
-  //   return mappedForecast;
-
-    
-  // });
-};
-  
-
-    
-    
-    
-
-
-    
-
-
-  
-  
-
-
-
-
-
-  // const eachCity = {};
-
-  // const mapEachCity = forecasts.map((forecast) => {
+// another thought...const mapEachCity = forecasts.map((forecast) => {
     //to do: map the 1st order filter to the 2nd order filter right here. Then, push the 2nd order filter to empty eachCity object. Then, return eachCity object.
+     
 
-    
+// originally what I had...this block of code pushes all city values into one array, all temperature values into another array, all pressure values into another array, and all humidity values into another array...
+  
 
+  const city = [];
+  
 
-  // }
- 
+  
 
-  // const renderCities = () => {
-  //   return forecasts.map((eachCity) => {
+  forecasts.reduce((acc, f) => {
+    if (acc.indexOf(f.city) === -1) {
+      acc.push(f.city);
+    }
+    return acc;
+  }, city);
 
+  console.log(forecasts);
 
+  let contents = city.map(c => {
+    //debugger;
+    const temp = [];
+    const pressure = [];
+    const humidity = [];
 
+    //filter forecast by the city using 'c' 
+    let filteredForecast = forecasts.filter(f => {
+      let include = f.city === c;
+    // debugger;
+      return include;
+    });
+    filteredForecast.forEach((f) => {
+      temp.push(f.temperature);
+      pressure.push(f.pressure);
+      humidity.push(f.humidity);
+  });
 
-  // forecasts.forEach((forecast) => {
-    
+    return (
+      <tr>
+                <td>{city}</td>
+                <td>
+                  <Sparklines data={temp} height={110} width={180} 
+                  >
+                    <SparklinesLine color="red" />
+                    <SparklinesReferenceLine type="avg" />
+                  </Sparklines>
+                </td>
+                <td>
+                  <Sparklines data={pressure} height={120} width={180}>
+                    <SparklinesLine color="green" />
+                    <SparklinesReferenceLine type="avg" />
+                  </Sparklines>
+                </td>
+                <td>
+                  <Sparklines data={humidity} height={120} width={180}>
+                    <SparklinesLine color="blue" />
+                    <SparklinesReferenceLine type="avg" />
+                  </Sparklines>
+                </td>
+              </tr>
+  )})
+  
 
-  // this block of code is just brainstorming...not stubbed out yet
-  // forecasts.map(val, id) => {
-  //   return (
-  //     <tr key={val.id}>
-  //       <td>{val.city}</td>
-  //       <td>
-  //         <Sparklines data={val.temperature} height={120} width={180}>
-  //           <SparklinesLine color="red" />
-  //           <SparklinesReferenceLine type="avg" />
-  //         </Sparklines>
-  //       </td>
-  //       <td>
-  //         <Sparklines data={val.pressure} height={120} width={180}>
-  //           <SparklinesLine color="blue" />
-  //           <SparklinesReferenceLine type="avg" />
-  //         </Sparklines>
-  //       </td>
-  //       <td>
-  //         <Sparklines data={val.humidity} height={120} width={180}>
-  //           <SparklinesLine color="green" />
-  //           <SparklinesReferenceLine type="avg" />
-  //         </Sparklines>
-  //       </td>
-  //     </tr>
-  //   );
-  // };
+  return (
 
-
-// this block of code pushes all city values into one array, all temperature values into another array, all pressure values into another array, and all humidity values into another array...
-//   const city = [];
-//   const temp = [];
-//   const pressure = [];
-//   const humidity = [];
-
-//   forecasts.forEach((f) => {
-//     temp.push(f.temperature);
-//     pressure.push(f.pressure);
-//     humidity.push(f.humidity);
-//   });
-
-//   forecasts.reduce((acc, f) => {
-//     if (acc.indexOf(f.city) === -1) {
-//       acc.push(f.city);
-//     }
-//     return acc;
-//   }, city);
-
-//   console.log(forecasts);
-
-
-
-//   return (
-
-//     <div>
-//       <table className="table table-hover" id="display">
-//         <thead>
-//           <tr key= {city}>
-//             <th>City</th>
-//             <th>Temperature (F)</th>
-//             <th>Pressure (hPa)</th>
-//             <th>Humidity (%)</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {
-//               <tr>
-//                 <td>{city}</td>
-//                 <td>
-//                   <Sparklines data={temp} height={110} width={180} 
-//                   >
-//                     <SparklinesLine color="red" />
-//                     <SparklinesReferenceLine type="avg" />
-//                   </Sparklines>
-//                 </td>
-//                 <td>
-//                   <Sparklines data={pressure} height={120} width={180}>
-//                     <SparklinesLine color="green" />
-//                     <SparklinesReferenceLine type="avg" />
-//                   </Sparklines>
-//                 </td>
-//                 <td>
-//                   <Sparklines data={humidity} height={120} width={180}>
-//                     <SparklinesLine color="blue" />
-//                     <SparklinesReferenceLine type="avg" />
-//                   </Sparklines>
-//                 </td>
-//               </tr>
-//           }
-//         </tbody> 
-//       </table>
-//     </div>
-//   );
-// } 
+    <div>
+     
+      <table className="table table-hover" id="display">
+        <thead>
+          <tr key= {city}>
+            <th>City</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contents}
+        </tbody> 
+      </table>
+    </div>
+  );
+} 
 
 export default CitiesShow;
 
