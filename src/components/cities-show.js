fd649474c75fbf698335a39f27676d5c76c718a7
RyanCaimano <ryancaimano@gmail.com>
2022-09-14 14:02:35 -0500

    Created math methods for computing the average temp, pressure, humidity. Fully functional and renders to page. Need to make sure the averages are displayed in the center by modifying it in index.css. The code for the math methods currently reside within CitiesShow, but thinking of placing it somewhere else and including a hook...just to avoid clutter.

--- src/components/cities-show.js
+++ src/components/cities-show.js
@@ -15,24 +15,19 @@ const CitiesShow = () => {
     if (acc.indexOf(f.city) === -1) {
       acc.push(f.city);
     }
-  
-    console.log(f.city);
     return acc;
   }, city);
 
-  console.log(city[0]);
-
   let contents = city.map((c) => {
-    //this assures that the city will not render twice if it is searched for twice
+    //defined 'name' in a way that assures that the city will not render twice if it is searched for twice
     let name = city[city.indexOf(c)];
     let temp = [];
     let pressure = [];
     let humidity = [];
-
-    //filter forecast by the city using 'c'
+   
+    //filter forecast by the city using 'c' ...returns a boolean
     let filteredForecast = forecasts.filter((f) => {
       let include = f.city === c;
-      // debugger;
       return include;
     });
 
@@ -42,6 +37,31 @@ const CitiesShow = () => {
       humidity.push(f.humidity);
     });
 
+    console.log(forecasts);
+
+    let averageTemp = Math.round(
+      temp.reduce((acc, t) => {
+        return acc + t;
+      }, 0) / temp.length
+    );
+
+    let averagePressure = Math.round(
+      pressure.reduce((acc, p) => {
+        return acc + p;
+      }, 0) / pressure.length
+    );
+
+    let averageHumidity = Math.round(
+      humidity.reduce((acc, h) => {
+        return acc + h;
+      }, 0) / humidity.length
+    );
+
+
+
+
+
+
     return (
       <tr key="{city.name}">
         <td>{name}</td>
@@ -49,19 +69,22 @@ const CitiesShow = () => {
           <Sparklines data={temp} height={110} width={180}>
             <SparklinesLine color="red" />
             <SparklinesReferenceLine type="avg" />
-          </Sparklines>
+          </Sparklines><br/>
+          <div>{averageTemp} F</div>
         </td>
         <td>
           <Sparklines data={pressure} height={120} width={180}>
             <SparklinesLine color="green" />
             <SparklinesReferenceLine type="avg" />
-          </Sparklines>
+          </Sparklines><br/>
+          <div>{averagePressure} hPa</div>
         </td>
         <td>
           <Sparklines data={humidity} height={120} width={180}>
             <SparklinesLine color="blue" />
             <SparklinesReferenceLine type="avg" />
-          </Sparklines>
+          </Sparklines><br/>
+          <div>{averageHumidity} %</div>
         </td>
       </tr>
     );
