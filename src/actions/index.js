eb5703f368f1a45a9ac5ecaeb2eb77232b2955ac
RyanCaimano <ryancaimano@gmail.com>
2022-09-16 09:15:22 -0500

    Modified fetchForecast function in actions, accounting for code on commmands that are not found. Next up is modifying my reducer with an additonal case for error.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
 export const FETCH_FORECAST = 'FETCH_FORECAST';
+export const FETCH_FORECAST_ERROR = 'FETCH_FORECAST_ERROR';
 const API_KEY = '9de0841aea702821eece6900aab8d8f1';
 const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
+
 export async function fetchForecast(city) {
   const request = await axios.get(`${API_URL}&q=${city}`);
-  return {
-    type: FETCH_FORECAST,
-    payload: request
-  };
+  const error = 'This city was not found';
+
+  if (request.data.cod === '404') {
+    return {
+      type: FETCH_FORECAST_ERROR,
+      payload:{error}
+    };
+  } else {
+    return {
+      type: FETCH_FORECAST,
+      payload: request
+  }}; 
 }
-
-
