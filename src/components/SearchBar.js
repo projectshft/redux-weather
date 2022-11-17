a32c88c0f01cbe7bcfd2a77e86d94688fbd7b5ea
unknown <dgoodenough@proton.me>
2022-11-17 11:36:38 -0800

    Layout the search bar with a button for searching the input or using current location

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,29 @@
+import { Row, Col, InputGroup, Form } from 'react-bootstrap';
+
 const SearchBar = () => (
-  <div>
-    <input placeholder="Enter a city for weather data" />
-  </div>
+  <Row className="justify-content-center ">
+    <Col md={6}>
+      <InputGroup className="my-3">
+        <InputGroup.Text
+          role="button"
+          type="submit"
+          className="btn btn-primary"
+          id="search-current-location"
+        >
+          CL
+        </InputGroup.Text>
+        <Form.Control placeholder="Enter a city to find weather information..." />
+        <InputGroup.Text
+          role="button"
+          type="submit"
+          className="btn btn-primary"
+          id="search-button"
+        >
+          Search
+        </InputGroup.Text>
+      </InputGroup>
+    </Col>
+  </Row>
 );
 
 export default SearchBar;
