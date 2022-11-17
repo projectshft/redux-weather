1f71b349144aa70992a06565acb0c07643b42d27
unknown <dgoodenough@proton.me>
2022-11-17 11:08:14 -0800

    Resolve linting issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import Header from './Header';
-import Searchbar from './SearchBar';
+import SearchBar from './SearchBar';
 import Weather from '../containers/Weather';
 
 function App() {
   return (
-  <div className="App">
-    <Header />
-    <SearchBar />
-    <Weather />
-  </div>);
+    <div className="App">
+      <Header />
+      <SearchBar />
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
