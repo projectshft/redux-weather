aae368af5f21c833e6e184f456c58d4fd9b07ad8
unknown <dgoodenough@proton.me>
2022-11-17 10:56:45 -0800

    Layout components to be rnedered into the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import Header from './Header';
+import Searchbar from './SearchBar';
+import Weather from '../containers/Weather';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+  <div className="App">
+    <Header />
+    <SearchBar />
+    <Weather />
+  </div>);
 }
 
 export default App;
