af04a41c3bfb40c35ebf3af22e23beb09248c1f2
unknown <dgoodenough@proton.me>
2022-11-17 11:16:19 -0800

    Add in react-bootstrap to create a header that is centered

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 
 const Header = () => (
-  <div>
-    <div>Weather</div>
-  </div>
+  <Navbar bg="dark" expand="lg" variant="dark" fixed="top">
+    <Container className="justify-content-center">
+      <Navbar.Brand href="#home">Awesomer Weather</Navbar.Brand>
+    </Container>
+  </Navbar>
 );
 
 export default Header;
