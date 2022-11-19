7878500c0cdd0078922a180d6180f53fd9e0586a
unknown <dgoodenough@proton.me>
2022-11-19 08:52:45 -0800

    Remove everything for formatting the times

--- src/containers/WeatherDataListItem.js
+++ src/containers/WeatherDataListItem.js
@@ -17,23 +17,6 @@ const WeatherDataListItem = ({ weather }) => {
   const timeData = formatTime(weather.forecast);
   console.log(timeData);
 
-  // const unixTimes = weather.forecast.map((time) => new Date(time.dt * 1000));
-  // const hrs = unixTimes.map((time) => time.getHours());
-
-  // const formattedHrs = hrs.map((hour) => {
-  //   if (hour <= 12) {
-  //     return `${hour}AM`;
-  //   }
-  //   return `${hour - 12}PM`;
-  // });
-
-  // const days = unixTimes.map((time) => time.getDay());
-
-  // const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-
-  // const formattedDays = days.map((day) => daysOfWeek[day]);
-  // const timeData = { formattedDays, formattedHrs };
-
   const tempData = weather.forecast.map((dataPoint) => dataPoint.main.temp);
   const pressureData = weather.forecast.map(
     (dataPoint) => dataPoint.main.pressure
