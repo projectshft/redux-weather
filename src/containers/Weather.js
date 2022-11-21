afbda85c0e282ffd274ec44d0adcf00f9909d77f
unknown <dgoodenough@proton.me>
2022-11-21 08:52:12 -0800

    Remove useEffect, it was not doing anything

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -1,17 +1,9 @@
-import { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import _ from 'lodash';
 import WeatherDataListItem from './WeatherDataListItem';
 
 const Weather = () => {
-  const [defaultWeather, setDefaultWeather] = useState({});
-
-  useEffect(() => {
-    const storedDefault = JSON.parse(localStorage.getItem('defaultWeather'));
-    console.log(storedDefault);
-  }, []);
-
   const weatherData = useSelector((state) => state.weather);
   const renderWeatherDataList = () =>
     _.map(weatherData, (obj, key) => {
