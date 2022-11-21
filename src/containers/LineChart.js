57b595f77dd843f655ac525ec154de3ca563ea70
unknown <dgoodenough@proton.me>
2022-11-21 06:50:49 -0800

    Add a modal for the line chart

--- src/containers/LineChart.js
+++ src/containers/LineChart.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProptTypes from 'prop-types';
-import { Col } from 'react-bootstrap';
+import { Col, Modal } from 'react-bootstrap';
 // eslint-disable-next-line import/no-unresolved
 import { Line } from 'react-chartjs-2';
 import {
@@ -28,12 +28,37 @@ ChartJS.register(
   annotationPlugin
 );
 
-const LineChart = ({ config }) => (
-  <Col md={3}>
-    <Line className="chart-small" data={config.data} options={config.options} />
-  </Col>
-);
+const LineChart = ({ config }) => {
+  const [show, setShow] = useState(false);
 
+  const handleShow = () => setShow(true);
+  const handleHide = () => setShow(false);
+  return (
+    <>
+      <Col md={3}>
+        <Line
+          onClick={handleShow}
+          className="chart-small"
+          data={config.data}
+          options={config.options}
+        />
+      </Col>
+      
+      <Modal show={show} onHide={handleHide} centered size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{config.options.plugins.title.text}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Line
+            className="chart-modal"
+            data={config.data}
+            options={{ ...config.options, maintainAspectRatio: true }}
+          />
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
 LineChart.propTypes = {
   config: ProptTypes.object,
 };
