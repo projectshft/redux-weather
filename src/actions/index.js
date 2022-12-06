fd321a99c3d568807c3dc6cd830d6a136dc14ec7
Carmen Chow <carmendevelops@gmail.com>
2022-12-05 23:27:47 -0500

    fetch data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,14 @@
-export function fetchWeather(city){
+import axios from 'axios'
 
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=75e7ccabdef5725374998f0c3f3798b2`;
 
-  return function(dispatch){
-    fetch(url)  
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      dispatch({
-        type: 'GET_WEATHER',
-        payload: data
-      }).catch(err => {
-        console.log(err);
-      })
-    })
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
