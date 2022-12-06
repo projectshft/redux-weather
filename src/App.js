0dba8c1ba6c42f3586f33c322d18312ed6f11fd1
funkee <xxtrm98@protonmail.com>
2022-12-06 11:45:46 -0800

    added sparklines chart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesSpots,
+  SparklinesReferenceLine,
+} from "react-sparklines";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-//Redux state access and dispatch
+import Table from "react-bootstrap/Table";
 import { useSelector, useDispatch } from "react-redux";
-//React Hooks
 import { useState } from "react";
-// action creators
 import { updateAPI } from "../src/redux/city.js";
 
 function App() {
   const [cityName, setCityName] = useState("");
   const cityProfile = useSelector((state) => state);
 
+  // We will map through the dynamic list of cities added and populate the data from Redux store.
   const listedCities = cityProfile.citySearchReducer.weatherInfo.map(
     (item, index) => {
       return (
@@ -19,12 +25,31 @@ function App() {
             <b>{item.city}</b>
           </td>
           <td>
-            <b>{item.temp} °F</b>
+            <div className="">
+              <Sparklines data={item.fiveDayTemp}>
+                <SparklinesLine color="#56b45d" />
+                <SparklinesSpots style={{ fill: "#56b45dn" }} />
+                <SparklinesReferenceLine type="mean" />
+              </Sparklines>
+              <b>{item.temp} °F</b>
+            </div>
           </td>
           <td>
-            <b>{item.pressure} hPa</b>
+            <div className="column-style">
+              <Sparklines data={item.fiveDayPressure}>
+                <SparklinesLine color="#56b45d" />
+                <SparklinesSpots style={{ fill: "#56b45d" }} />
+                <SparklinesReferenceLine type="mean" />
+              </Sparklines>
+              <b>{item.pressure} hPa</b>
+            </div>
           </td>
           <td>
+            <Sparklines data={item.fiveDayHumidity}>
+              <SparklinesLine color="#56b45d" />
+              <SparklinesSpots style={{ fill: "#56b45d" }} />
+              <SparklinesReferenceLine type="mean" />
+            </Sparklines>
             <b>{item.humidity} %</b>
           </td>
         </tr>
@@ -32,10 +57,12 @@ function App() {
     }
   );
 
+  // declaring dispatch to have access the action creators we exported in the redux folder.
   const dispatch = useDispatch();
   const getCurrentWeatherInfoAction = (cityName) =>
     dispatch(updateAPI(cityName));
 
+  // This will execute the form search and place search data into the Redux dispatch function as well as clear the input field.
   const getCurrentWeatherInfo = (e) => {
     e.preventDefault();
     if (cityName === "") {
@@ -46,7 +73,6 @@ function App() {
     }
   };
 
-  // api reference is ----> process.env.REACT_APP_API_KEY
   return (
     <div className="App">
       <div>
@@ -57,26 +83,17 @@ function App() {
             onChange={(e) => setCityName(e.target.value)}
           ></input>
         </form>
-        <table className="table table-hover">
+        <Table striped bordered hover variant="dark">
           <thead>
             <tr>
               <th scope="col">City</th>
-              <th scope="col">Temperature</th>
-              <th scope="col">Pressure</th>
-              <th scope="col">Humidity</th>
+              <th scope="col">Temperature(F)</th>
+              <th scope="col">Pressure(hPa)</th>
+              <th scope="col">Humidity(%)</th>
             </tr>
           </thead>
-          <tbody>
-            {/* <tr>
-              <td></td>
-              <td></td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr> */}
-
-            {listedCities}
-          </tbody>
-        </table>
+          <tbody>{listedCities}</tbody>
+        </Table>
       </div>
     </div>
   );
--- src/redux/city.js
+++ src/redux/city.js
@@ -8,61 +8,54 @@ export function updateAPI(cityName) {
         `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
       )
       .then((response) => {
-        // console.log(response.data);
-        // console.log("the pressure is:", response.data.list[0].main.pressure);
-        // console.log("the humidity is:", response.data.list[0].main.humidity);
-        // console.log("the 5 day forecast is:");
-        // console.log(response.data.list[0].main.temp);
-        // console.log(response.data.list[8].main.temp);
-        // console.log(response.data.list[16].main.temp);
-        // console.log(response.data.list[24].main.temp);
-        // console.log(response.data.list[32].main.temp);
-
         dispatch({ type: "GET_CITY", payload: response.data });
       })
       .catch((error) => {
         console.log(error);
-        alert("Please try again!");
+        alert("Error! Please try again!");
       });
   };
 }
 
-const otherInitialState = {
-  weatherInfo: [
-    // {
-    //   city: "Sandusky",
-    //   temp: 45,
-    //   pressure: 87,
-    //   humidity: 40,
-    // },
-  ],
+// initial state my reducer function will utilize to build weather object.
+const initialState = {
+  weatherInfo: [],
 };
 
-// state
-// const initialState = {
-//   weatherInfo: [testObj, testObj1],
-// };
-
-//previous set up for reducer state to concatenate weather data.
-// city: weather.city.concat([action.payload])
-
-// weatherInfo: [...weather.weatherInfo, action.payload],
-
 // reducerFunction to update weather details.
-
-export default function citySearchReducer(weather = otherInitialState, action) {
+export default function citySearchReducer(weather = initialState, action) {
   switch (action.type) {
     case "GET_CITY":
       // console.log(action.payload);
       return {
-        // ...weather,
         weatherInfo: [
           ...weather.weatherInfo,
           {
             city: action.payload.city.name,
             temp: action.payload.list[0].main.temp,
+            fiveDayTemp: [
+              action.payload.list[0].main.temp,
+              action.payload.list[8].main.temp,
+              action.payload.list[16].main.temp,
+              action.payload.list[24].main.temp,
+              action.payload.list[32].main.temp,
+            ],
             pressure: action.payload.list[0].main.pressure,
+            fiveDayPressure: [
+              action.payload.list[0].main.pressure,
+              action.payload.list[8].main.pressure,
+              action.payload.list[16].main.pressure,
+              action.payload.list[24].main.pressure,
+              action.payload.list[32].main.pressure,
+            ],
             humidity: action.payload.list[0].main.humidity,
+            fiveDayHumidity: [
+              action.payload.list[0].main.humidity,
+              action.payload.list[8].main.humidity,
+              action.payload.list[16].main.humidity,
+              action.payload.list[24].main.humidity,
+              action.payload.list[32].main.humidity,
+            ],
           },
         ],
       };
