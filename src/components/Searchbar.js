c174dad7b5103e3533f1d520bf9b33731c3cbf19
Carmen Chow <carmendevelops@gmail.com>
2022-12-05 23:41:52 -0500

    add Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,26 +1,34 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux;'
-import { store } from './store'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { fetchWeather } from './redux/reducers'
 
 const Searchbar = () => {
   const [city, setCity] = useState('');
-  const [weather, setWeather] = useState('');
+  // const [weather, setWeather] = useState('');
   const dispatch = useDispatch();
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=75e7ccabdef5725374998f0c3f3798b2`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=75e7ccabdef5725374998f0c3f3798b2`;
  
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
   const handleSearch = (e) => {
-    if (e.key === 'Enter') {
-      fetch(url)
-       .then(response => response.json())
-       .then(data => {
-        console.log(data)
-        setWeather(data);
-        setCity('');
-      })
-    }
-  }
+    e.preventDefault();
+    dispatch(fetchWeather(city));
+  };
+  
+  //   if (e.key === 'Enter') {
+  //     fetch(url)
+  //      .then(response => response.json())
+  //      .then(data => {
+  //       console.log(data)
+  //       setWeather(data);
+  //       setCity('');
+  //     })
+  //   }
+  // }
 
   return (
     <div>
@@ -29,9 +37,10 @@ const Searchbar = () => {
           type="text"
           name="city"
           value={city}
-          onChange={(e) => setCity(e.target.value)}
+          onChange={handleChange}
           placeholder="Search city"
         />
+        <button type="submit">Search</button>
       </form>
     </div>
   )
