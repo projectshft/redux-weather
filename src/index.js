d44c0e2dce5466cb8c927704d139e88313f5c40f
Daniel Bui <dnlbui@proton.me>
2022-12-07 17:01:38 -0600

    App and index boiler plate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+  document.getElementById('root')
+);
