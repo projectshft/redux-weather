67fce72e981931276c5f8a37c7630e3c85cc4c65
Daniel Bui <dnlbui@proton.me>
2022-12-08 12:57:07 -0600

    set up header and store skeleton

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+
+const Header = (props) => {
+  return (
+    <div>
+      <div className='jumbotron text-center'>
+        <div className="container">
+          <h1 className="jumbotron-heading">Redux Weather</h1>
+        </div>
+      </div>
+      <div className='container'>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import Header from './components/header.js'
 import App from './App';
+import reducers from './reducers'
+
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>
+    <Header>
+      <App component={App}/>
+    </Header>
+  </Provider>,
   document.getElementById('root')
 );
