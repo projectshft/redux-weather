de5b6eab680fb00d3e0c390deb5d29aaee3043a6
Daniel Bui <dnlbui@proton.me>
2022-12-12 16:59:12 -0600

    trying to get weather index to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "./container/search_bar";
+import WeatherIndex from "./container/weather_index"
 
 
 const App = () =>{
   return(
     <div className="col-md-6 offset-md-3">
       <SearchBar />
-      {/* <WeatherIndex> */}
+      <br></br>
+      <WeatherIndex />
     </div>
   )
 }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,8 +5,9 @@ import {FETCH_CITYWEATHER} from "../actions";
 const weatherPostsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_CITYWEATHER:
-      console.log(action.payload);
-      return [action.payload];
+      console.log([action.payload.data,...state])
+      debugger;
+      return [action.payload.data,...state];
     default:
       return state;
   }
