1c15360f959f1d1b56f984e57a36df47e92a6d13
Daniel Bui <dnlbui@proton.me>
2022-12-14 13:32:56 -0600

    added a catch

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,12 @@ const ROOT_URL = "https://api.openweathermap.org/data/2.5";
 
 //NOT SURE HOW TO DO ERROR, TRY , CATCH
 export function fetchCityWeather(city) {
-  const request = axios.get(`${ROOT_URL}/forecast?q=${city}&appid=d2ebbe84afd80c9a2267505ea9c93841`);
+ 
+  const request = axios.get(`${ROOT_URL}/forecast?q=${city}&appid=d2ebbe84afd80c9a2267505ea9c93841`)
+  .catch(err=> {
+    console.log(`City not found. Input a valid city! Err code: ${err}`)
+  })
+
   return {
     type: FETCH_CITYWEATHER,
     payload: request,
