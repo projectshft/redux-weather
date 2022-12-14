923b36bda73aa1f17a38df12ac2e287c2c959d34
Carmen Chow <carmendevelops@gmail.com>
2022-12-14 02:04:06 -0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/searchBar'
+import searchBar from './components/searchBar';
+import WeatherGraphs from './components/WeatherGraphs';
 
 function App() {
   return (
     <div className="App">
       <h1>Redux Weather App</h1>
+      <searchBar/>
       <WeatherDisplay/>
     </div>
   );
---
+++ src/redux/index.js
@@ -0,0 +1,11 @@
+
+import { combineReducers } from 'redux';
+import WeatherReducer from './WeatherReducer'
+
+const rootReducer = combineReducers({
+  weather: WeatherReducer
+});
+
+export default rootReducer;
+
+
