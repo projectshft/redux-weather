e6698ce8fa7eaa0f5371979815ab5ed95c1d1d0c
Daniel Bui <dnlbui@proton.me>
2022-12-14 14:42:53 -0600

    changed spacing

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -6,15 +6,18 @@ const weatherPostsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_CITYWEATHER: 
       const weatherData = action.payload.data;
+
       const fiveDayData = (data) => {
         const temp = [];
         const pressure = [];
         const humidity = [];
+
         for (let i = 0; i < action.payload.data.list.length; i+=8) {
           temp.push(action.payload.data.list[i].main.temp);
           pressure.push(action.payload.data.list[i].main.pressure);
           humidity.push(action.payload.data.list[i].main.humidity);
         };
+
         return({
           city: action.payload.data.city.name.toUpperCase(),
           temp: temp,
@@ -23,6 +26,7 @@ const weatherPostsReducer = (state = [], action) => {
         })
       }; 
       return [fiveDayData(weatherData),...state];
+    
     default:
       return state;
   }
