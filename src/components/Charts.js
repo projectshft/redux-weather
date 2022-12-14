5d94c71e33edd67742243f918632f2305000760a
Carmen Chow <carmendevelops@gmail.com>
2022-12-14 18:16:47 -0500

    get average temp, pressure, humitidy

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -2,37 +2,55 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-function Charts () {
-  const weatherCharts = useSelector(state => state.weatherCharts)
+function Charts(){
+  const chartInfo = useSelector((state) => state.chartInfo)
+  const displayCharts = () => {
+    return chartInfo.map((chartInfo, id) => {
+    
+    const getAverageData = (array) => {
+      const total = array.reduce((acc, val) => acc + val, 0) / array.length;
+      return Math.round(total);
+    }
 
-  const displaySparklines = () => {
-    // if(weatherCharts){
-      return weatherCharts.map((chart, index => {
+    const averageTemp = getAverageData(weather.temp);
+    const averagePressure = getAverageData(weather.pressure);
+    const averageHumidity = getAverageData(weather.humidity);
+
+
+  return(
     <tr key={index}>
       <th>{currentWeatherPost.city}</th>
       <td >
-        <Sparklines limit={5} svgWidth={250} svgHeight={75} margin={5} data={currentWeatherPost.temp}>
-          <SparklinesLine />
-          <SparklinesReferenceLine type="mean" />
+        <Sparklines data={temp} height={100} width={120}>
+          <SparklinesLine color="orange" />
+          <SparklinesReferenceLine type="avg" />
         </Sparklines>
+        <p>{averageTemp} F</p>
       </td>
-      <td >
-        <Sparklines  limit={5} svgWidth={250} svgHeight={75} margin={5} data={currentWeatherPost.pressure}>
-          <SparklinesLine />
-          <SparklinesReferenceLine type="mean" />
+
+     <td >
+        <Sparklines data={pressure} height={100} width={120}>
+          <SparklinesLine color="green" />
+          <SparklinesReferenceLine type="avg" />
         </Sparklines>
+        <p>{averagePressure} hPa</p>
       </td>
+
       <td >
-        <Sparklines limit={5} svgWidth={250} svgHeight={75} margin={5} data={currentWeatherPost.humidity}>
-          <SparklinesLine />
-          <SparklinesReferenceLine type="mean" />
+        <Sparklines data={humidity} height={100} width={120}>
+          <SparklinesLine color="blue" />
+          <SparklinesReferenceLine type="avg" />
         </Sparklines>
+        <p>{averageHumidity} %</p>
       </td>
     </tr>
-  );
+    );
+  });
+}
+
+return (
+  displayCharts ()
+)
 }
 
 export default Charts
-
-      
-
