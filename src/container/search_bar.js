4ad97621c7d6817ec5fe8242440f225fb7998e19
Daniel Bui <dnlbui@proton.me>
2022-12-14 14:22:22 -0600

    Added comments

--- src/container/search_bar.js
+++ src/container/search_bar.js
@@ -3,16 +3,23 @@ import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash"
 import { fetchCityWeather } from "../actions";
 
-
+//funciton component that renders the searchbar
 const SearchBar = () => {
+  //useSelector function hook allows component to extract data from Redux store state
   const weatherPosts = useSelector(state => state.weatherPosts)
+
+  //usedispatch function hook returns dispatch function from redux store. Used to dispatch actions.
   const dispatch = useDispatch();
-  
+
+  //react local state
   const [cityState, setCity] = useState("")
-  
+
+  //function which handles onClick.
   const handleSearchButtonClick = (e) => {
     e.preventDefault();
+    //variable which searches for input value inside of an array of objects. Returns in an arraay, matched objects that contain value to input.
     const filteredCity = _.filter(weatherPosts, (obj) => e.target.value.toUpperCase() === obj.city)
+    //if checking for input matches to redux state
     if(weatherPosts.length === 0){
       dispatch(fetchCityWeather(e.target.value));
     }
@@ -20,7 +27,9 @@ const SearchBar = () => {
       dispatch(fetchCityWeather(e.target.value));
     }else if(filteredCity.length !== 0){
       return alert("City already listed!");
-    }else{
+    }
+    //does not execute because error. Not sure how to keep program running with failed promise from axios.
+    else{
       return alert("Enter a valid city...")
     }
     
--- src/container/weather_list.js
+++ src/container/weather_list.js
@@ -3,8 +3,10 @@ import { Sparklines,SparklinesLine,SparklinesReferenceLine } from 'react-sparkli
 
 //component which returns list of weather that contains 5 day charts for temp, pressure, and humidity
 const WeatherList = () =>{
+  //useSelector function allows component to extract data from Redux store state
   const weatherPosts = useSelector(state => state.weatherPosts)
 
+  //function used to return JSX for the charts that takes the 5 day data values
   const renderChart = (dataValues) => {
     return(
       <td >
@@ -16,6 +18,7 @@ const WeatherList = () =>{
     )
   }
 
+  //function used return JSX row that contains all the charts and name of the city. Only render if state has objects.
   const RenderRow = () => { 
     if(weatherPosts.length>0){
       return weatherPosts.map((currentWeatherPost, index)=>{
