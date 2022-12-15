fcd410e15cfbf34684d456cf6e4663b9d39813f7
Daniel Bui <dnlbui@proton.me>
2022-12-14 21:51:38 -0600

    added an api variable. Tried to do local..

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,22 @@
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-//const API_KEY = process.env.REDUX_APP_WEATHER_API_KEY
 import axios from "axios";
 import _ from "lodash"
 
 export const FETCH_CITYWEATHER = "FETCH_CITYWEATHER";
-
+//const API_KEY = process.env.REDUX_APP_WEATHER_API_KEY;
 const ROOT_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "d2ebbe84afd80c9a2267505ea9c93841";
 
 //NOT SURE HOW TO PROCEED FROM ERROR
 //function which returns type and payload
 export function fetchCityWeather(city) {
   //promise and catch
-  const request = axios.get(`${ROOT_URL}/forecast?q=${city}&appid=d2ebbe84afd80c9a2267505ea9c93841&units=imperial`)
+  const request = axios.get(`${ROOT_URL}/forecast?q=${city}&units=imperial&appid=${API_KEY}`)
   .then(request=> {
     const fiveDayData = (data) => {
       const temp = [];
       const pressure = [];
       const humidity = [];
+
       data.data.list.forEach(element => {
         temp.push(element.main.temp);
         pressure.push(element.main.pressure);
