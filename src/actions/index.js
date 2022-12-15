abb5f94b8472b453ee736caf1f5af2930f6af209
Daniel Bui <dnlbui@proton.me>
2022-12-14 19:02:12 -0600

    moved api data manipulationa + lodash to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 //const API_KEY = process.env.REDUX_APP_WEATHER_API_KEY
 import axios from "axios";
+import _ from "lodash"
 
 export const FETCH_CITYWEATHER = "FETCH_CITYWEATHER";
 
@@ -11,6 +12,30 @@ const ROOT_URL = "https://api.openweathermap.org/data/2.5";
 export function fetchCityWeather(city) {
   //promise and catch
   const request = axios.get(`${ROOT_URL}/forecast?q=${city}&appid=d2ebbe84afd80c9a2267505ea9c93841&units=imperial`)
+  .then(request=> {
+    const fiveDayData = (data) => {
+      const temp = [];
+      const pressure = [];
+      const humidity = [];
+      data.data.list.forEach(element => {
+        temp.push(element.main.temp);
+        pressure.push(element.main.pressure);
+        humidity.push(element.main.humidity);
+      })
+
+      return({
+        city: request.data.city.name.toUpperCase(),
+        temp: temp,
+        pressure: pressure,
+        humidity: humidity,
+        tempAve: _.round(_.mean(temp)),
+        pressureAve: _.round(_.mean(pressure)),
+        humidityAve: _.round(_.mean(humidity))
+      })
+    }
+    return fiveDayData(request)
+    
+  })
   .catch(err=> {
     console.log(`City not found. Input a valid city! ${err}`)
     alert(`City not found. Input a valid city! \n${err}`)
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,5 @@
 import {FETCH_CITYWEATHER} from "../actions";
-import _ from "lodash"
+
 
 //list of reducers which return an array of objects that contain the 5 day weather for a city.
 const weatherPostsReducer = (state = [], action) => {
@@ -9,35 +9,8 @@ const weatherPostsReducer = (state = [], action) => {
         return state;
       }
       else{
-        const weatherData = action.payload.data.list;
-  
-        const fiveDayData = (data) => {
-          const temp = [];
-          const pressure = [];
-          const humidity = [];
-
-          data.forEach(element => {
-            temp.push(element.main.temp);
-            pressure.push(element.main.pressure);
-            humidity.push(element.main.humidity);
-          })
-  
-          return({
-            city: action.payload.data.city.name.toUpperCase(),
-            temp: temp,
-            pressure: pressure,
-            humidity: humidity,
-            tempAve: _.round(_.mean(temp)),
-            pressureAve: _.round(_.mean(pressure)),
-            humidityAve: _.round(_.mean(humidity))
-          })
-        }; 
-
-        return [fiveDayData(weatherData),...state];
-        
+        return [action.payload,...state];
       }
-      
-    
     default:
       return state;
   }
