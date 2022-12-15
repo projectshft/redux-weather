ea128629f30c980311b451ca52a3e3f389e89ffe
Daniel Bui <dnlbui@proton.me>
2022-12-14 18:17:36 -0600

    all 40 data points in array. added ave and rounded

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,28 +1,31 @@
 import {FETCH_CITYWEATHER} from "../actions";
-
+import _ from "lodash"
 
 //list of reducers which return an array of objects that contain the 5 day weather for a city.
 const weatherPostsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_CITYWEATHER: 
-      const weatherData = action.payload.data;
+      const weatherData = action.payload.data.list;
 
       const fiveDayData = (data) => {
         const temp = [];
         const pressure = [];
         const humidity = [];
 
-        for (let i = 0; i < action.payload.data.list.length; i+=8) {
-          temp.push(action.payload.data.list[i].main.temp);
-          pressure.push(action.payload.data.list[i].main.pressure);
-          humidity.push(action.payload.data.list[i].main.humidity);
-        };
+        data.forEach(element => {
+          temp.push(element.main.temp);
+          pressure.push(element.main.pressure);
+          humidity.push(element.main.humidity);
+        })
 
         return({
           city: action.payload.data.city.name.toUpperCase(),
           temp: temp,
           pressure: pressure,
           humidity: humidity,
+          tempAve: _.round(_.mean(temp)),
+          pressureAve: _.round(_.mean(pressure)),
+          humidityAve: _.round(_.mean(humidity))
         })
       }; 
       return [fiveDayData(weatherData),...state];
