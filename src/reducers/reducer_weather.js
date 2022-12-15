ca496cf8ddbf4e0626c9b3ce36c73fee4649732e
Daniel Bui <dnlbui@proton.me>
2022-12-14 18:36:49 -0600

    added if else statement in reducer for errors

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,31 +4,39 @@ import _ from "lodash"
 //list of reducers which return an array of objects that contain the 5 day weather for a city.
 const weatherPostsReducer = (state = [], action) => {
   switch (action.type) {
-    case FETCH_CITYWEATHER: 
-      const weatherData = action.payload.data.list;
+    case FETCH_CITYWEATHER:
+      if(action.payload === undefined){
+        return state;
+      }
+      else{
+        const weatherData = action.payload.data.list;
+  
+        const fiveDayData = (data) => {
+          const temp = [];
+          const pressure = [];
+          const humidity = [];
 
-      const fiveDayData = (data) => {
-        const temp = [];
-        const pressure = [];
-        const humidity = [];
+          data.forEach(element => {
+            temp.push(element.main.temp);
+            pressure.push(element.main.pressure);
+            humidity.push(element.main.humidity);
+          })
+  
+          return({
+            city: action.payload.data.city.name.toUpperCase(),
+            temp: temp,
+            pressure: pressure,
+            humidity: humidity,
+            tempAve: _.round(_.mean(temp)),
+            pressureAve: _.round(_.mean(pressure)),
+            humidityAve: _.round(_.mean(humidity))
+          })
+        }; 
 
-        data.forEach(element => {
-          temp.push(element.main.temp);
-          pressure.push(element.main.pressure);
-          humidity.push(element.main.humidity);
-        })
-
-        return({
-          city: action.payload.data.city.name.toUpperCase(),
-          temp: temp,
-          pressure: pressure,
-          humidity: humidity,
-          tempAve: _.round(_.mean(temp)),
-          pressureAve: _.round(_.mean(pressure)),
-          humidityAve: _.round(_.mean(humidity))
-        })
-      }; 
-      return [fiveDayData(weatherData),...state];
+        return [fiveDayData(weatherData),...state];
+        
+      }
+      
     
     default:
       return state;
