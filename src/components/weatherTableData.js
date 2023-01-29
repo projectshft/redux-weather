6eace0d63a331eaf84b6d76dfc5f90fe2fa34de6
Nils Landsberg <nils.landsberg78@gmail.com>
2023-01-28 23:48:42 -0500

    WeatherTableData is working - can create a row from one city.  Need to create an array to store state on TableWeatherData.

--- src/components/weatherTableData.js
+++ src/components/weatherTableData.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Sparklines } from "react-sparklines";
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from "react-sparklines";
 import store from "../store/store";
 
 const WeatherTableData = () => {
   const cityInfo = useSelector((state) => state.forecasts)
-
+  // const tempAverage = cityInfo[temperature] => cityInfo.[temperature].reduce((a, b) => a + b) / cityInfo.temperature.length;
   console.log(cityInfo);
 
   return (
   <>
     { cityInfo ?
       <tr className="text-center">
-        <td>{cityInfo.city}</td>
-          <td>Temperature (K)</td>
-          <td>Pressure (hPa)</td>
-          <td>Humidity (%)</td>
+        <td className="align-middle">{cityInfo.city}</td>
+          <td>
+            <Sparklines data={cityInfo.temperature}>
+              <SparklinesLine color="orange" />
+              <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+          </td>
+          <td>
+          <Sparklines data={cityInfo.pressure}>
+              <SparklinesLine color="blue" />
+              <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+          </td>
+          <td>
+          <Sparklines data={cityInfo.humidity}>
+              <SparklinesLine color="blue" />
+              <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+          </td>
       </tr>
       : <tr></tr>
     }
