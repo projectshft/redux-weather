f0365103fe75948b02481609d7e6853930a30e8e
josephl37 <josephliow2000@gmail.com>
2023-02-23 15:28:24 -0600

    added table component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Search from "./components/search";
+import Table from "./components/table"
 
 function App() {
   return (
     <div className="col-md-6 offset-md-3">
       <Search />
+      <Table />
     </div>
   );
 }
---
+++ src/components/table.js
@@ -0,0 +1,26 @@
+const table = () => {
+  return (
+    <div>
+      <table class="table table-hover mt-3">
+        <thead>
+          <tr>
+            <th scope="col">City</th>
+            <th scope="col">Temperature (F)</th>
+            <th scope="col">Pressure (hPa)</th>
+            <th scope="col">Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Austin</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default table;
