59f74adb27159b24fe2d74ff34311ec20817b70a
josephl37 <josephliow2000@gmail.com>
2023-02-23 16:29:13 -0600

    added react sparklines

--- src/components/table.js
+++ src/components/table.js
@@ -1,7 +1,10 @@
+import React from "react";
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from "react-sparklines";
+
 const table = () => {
   return (
     <div>
-      <table class="table table-hover mt-3">
+      <table className="table table-hover mt-3">
         <thead>
           <tr>
             <th scope="col">City</th>
@@ -12,10 +15,43 @@ const table = () => {
         </thead>
         <tbody>
           <tr>
-            <th scope="row">Austin</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <th scope="row" className="align-middle">Austin</th>
+            <td>
+              <Sparklines
+                data={[60, 73, 71, 65, 80, 73, 68]}
+                width={100}
+                height={60}
+                margin={5}
+              >
+                <SparklinesLine color="#ffc61a" />
+                <SparklinesReferenceLine type="mean" />
+              </Sparklines>
+              <p className="text-center">58 F</p>
+            </td>
+            <td>
+            <Sparklines
+                data={[80, 73, 71, 49, 80, 73, 88]}
+                width={100}
+                height={65}
+                margin={5}
+              >
+                <SparklinesLine color="#00802b" />
+                <SparklinesReferenceLine type="mean" />
+              </Sparklines>
+              <p className="text-center">1013 hPa</p>
+            </td>
+            <td>
+            <Sparklines
+                data={[60, 73, 71, 65, 40, 53, 30]}
+                width={100}
+                height={70}
+                margin={5}
+              >
+                <SparklinesLine color="#253e56" />
+                <SparklinesReferenceLine type="mean" />
+              </Sparklines>
+              <p className="text-center">57 %</p>
+            </td>
           </tr>
         </tbody>
       </table>
