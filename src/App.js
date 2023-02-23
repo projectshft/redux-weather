0c27f3da682320251d3cc559bea1d5cad16ec2a6
josephl37 <josephliow2000@gmail.com>
2023-02-23 10:46:47 -0600

    set up input-field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
-
 function App() {
   return (
-    <div>
+    <div className="col-md-6 offset-md-3">
+      <form>
+        <div className="input-group mt-5">
+          <input
+            type="text"
+            className="form-control mb-2"
+            placeholder="Jane Doe"
+          />
+          <button type="submit" className="btn btn-primary mb-2">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
