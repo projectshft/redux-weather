8d727c2bcd755984547cd38828d9bddb8c2a4751
josephl37 <josephliow2000@gmail.com>
2023-02-25 19:13:29 -0600

    set up enviroment variable

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import axios from "axios"
-
+const API_KEY = process.env.REACT_APP_API_KEY;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
+
 export function fetchWeather(query) {
-  const request = axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=4df43207cfa7a1f67f6f7fbd99044f1c&units=imperial`)
+  const request = axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=imperial`)
 
   return {
     type: FETCH_WEATHER,
