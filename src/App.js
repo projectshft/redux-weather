036a4bc4d37687ba63030476e553eba95cc4ec92
Kelsey <kelseybailey55@gmail.com>
2023-05-25 09:41:04 -0600

    create containers and order

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from '.components/header';
+import CitySearch from '.components/city-search';
+import CreateDisplay from '.components/weather-display';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  <Container className='align-item-center'>
+    <Row className='ms-5'>
+      <Col>
+        <CitySearch />
+        <Header />
+        <CreateDisplay/>
+      </Col>
+    </Row>
+  </Container>
+  )
+
+
 }
+  
+    
 
 export default App;
