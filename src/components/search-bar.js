b90c1e5a169057e3a560b83f1edf30b3709018e9
William Ashley <williemays@Williams-MacBook-Air.local>
2023-05-27 09:20:41 -0400

    added yup resolver to display message if no city was entered

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,15 @@
 import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const postSchema = Yup.object().shape({
+  city: Yup.string().required()
+});
 
 const SearchBar = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(postSchema)
+  });
 
   const handleFormSubmit = (data) => {
     console.log(data);
@@ -12,9 +20,12 @@ const SearchBar = () => {
       <div className='form-group'>
         <label className='form-label'>Get a 5-day forecast</label>
       </div>
-      <div className='input-group mb-3'>
-        <input className='form-control' type='text' placeholder='Please enter a city' name='city' ref={register}></input>
-        <button className='btn btn-primary' type='submit'>Search</button>
+      <div className='mb-3'>
+        <div className='input-group'>
+          <input className='form-control' type='text' placeholder='Please enter a city' name='city' ref={register}></input>
+          <button className='btn btn-primary' type='submit'>Search</button>  
+        </div>
+        {errors.city?.message}
       </div>
       <hr />
     </form>
