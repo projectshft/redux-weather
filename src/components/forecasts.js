118d618739b1714d705a026b73b230671a38265c
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-07-22 09:56:01 -0700

    Sparklines is working but doesnt look like the example..

--- src/components/forecasts.js
+++ src/components/forecasts.js
@@ -1,14 +1,57 @@
 //This component will handle the table full of forecasts after you search a city
 
 import { useSelector } from 'react-redux/es/hooks/useSelector'
+import {
+    Sparklines,
+    SparklinesLine,
+    SparklinesReferenceLine,
+} from 'react-sparklines'
 
-function Forecasts() {
+const Forecasts = () => {
     const forecast = useSelector((state) => state.forecast)
     console.log(forecast)
 
+    //Loop through the list of forecasts and return each one formatted as a table row
+    const forecastListItems = forecast.map((cityForecast, index) => {
+        return (
+            <tr key={index}>
+                <td>{cityForecast.locationName}</td>
+                <td>
+                    <Sparklines data={cityForecast.temp}>
+                        <SparklinesLine />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines data={cityForecast.pressure}>
+                        <SparklinesLine />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines data={cityForecast.humidity}>
+                        <SparklinesLine />
+                        <SparklinesReferenceLine type="avg" />
+                    </Sparklines>
+                </td>
+            </tr>
+        )
+    })
+
     return (
-        <div>
-            <p>Forecasts Test</p>
+        //I need to make a table to hold the data.
+        <div className="tableWidth">
+            <table className="table table-striped table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">City</th>
+                        <th scope="col">Temperature</th>
+                        <th scope="col">Pressure</th>
+                        <th scope="col">Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>{forecastListItems}</tbody>
+            </table>
         </div>
     )
 }
