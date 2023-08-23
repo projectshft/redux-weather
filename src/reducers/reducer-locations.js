531751fcb216aa838ea9d060657bc3cc576ee3c3
etiry <7997357+etiry@users.noreply.github.com>
2023-08-23 14:01:29 -0400

    Normalize state

--- src/reducers/reducer-locations.js
+++ src/reducers/reducer-locations.js
@@ -1,11 +1,25 @@
-// import { normalize, schema } from 'normalizr';
+import { normalize, schema } from 'normalizr';
 // import _ from 'lodash';
 import { FETCH_COORDINATES } from "../actions";
 
-const locationsReducer = function (state = [], action) {
+const locationsSchema = new schema.Entity('locations', undefined, {
+  idAttribute: (value) => value.name
+});
+
+const DEFAULT_STATE = {
+  entries: {},
+  order: []
+}
+
+const locationsReducer = function (state = DEFAULT_STATE, action) {
   switch (action.type) {
     case FETCH_COORDINATES:
-      return [...state, [action.payload.data[0]]]
+      const normalizedLocation = normalize(action.payload.data[0], locationsSchema);
+
+      return {
+        entries: normalizedLocation.entities.locations,
+        order: [normalizedLocation.result]
+      }
     default:
       return state;
   }
