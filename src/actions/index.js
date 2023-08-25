7c55c870778009a91e785699e7fd4380fef9f804
etiry <7997357+etiry@users.noreply.github.com>
2023-08-25 17:17:26 -0400

    Move API key to env

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_LOCATION = 'FETCH_LOCATION';
 
-const apiKey = '94b98533e038ecd1e982b96426143136';
+const API_KEY = process.env.REACT_APP_API_KEY;
 const ROOT_URL = 'https://api.openweathermap.org';
 
 export function fetchLocation(query) {
@@ -19,9 +19,9 @@ export function fetchLocation(query) {
 }
 
 const fetchCoordinates = (query) =>
-  axios.get(`${ROOT_URL}/geo/1.0/direct?q=${query}&appid=${apiKey}`);
+  axios.get(`${ROOT_URL}/geo/1.0/direct?q=${query}&appid=${API_KEY}`);
 
 const fetchForecast = (lat, lon) =>
   axios.get(
-    `${ROOT_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+    `${ROOT_URL}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
   );
