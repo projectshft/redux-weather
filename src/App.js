b938c557e5ec4465a3fee087e5712c75b21a29a9
Scott Bouchard <holdwhatdoor@gmail.com>
2023-11-03 00:04:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+// weather api key
+const apiKey = 'e762b32d14efd802e7f067526402633f';
+// placeholder for URL search
+const searchInput = "";
+
+// current day and five day forecast URLs
+const todaysForecastURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}&units=imperial`;
+
+const fiveDayForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${apiKey}&units=imperial`;
+
+// placeholder sample date for Sparklines component
+const sampleData = [];
+
+// Sparklines mean chart component template
+const sparkLineMeanChart = (
+  <Sparklines data={sampleData}>
+      <SparklinesLine />
+      <SparklinesReferenceLine type="mean" />
+  </Sparklines>
+)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form class="search-form">
+          <div class="form-group">
+            <input type="text" id="search-query" class="form-control"     placeholder="City">
+            </input>
+            <button type="button" class="btn btn-primary search">Search</button>
+          </div>
+        </form>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './components/header'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header></Header>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
