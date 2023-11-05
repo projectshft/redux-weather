d80f71212ab033d8a0bbe081a8e7f7bf246a71fc
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2023-11-05 11:36:37 -0500

    ran npm install and fixes, created basic page layout

---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+const Home = (props) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8 offset-md-2 text-center">
+          <div className="page-header">
+          </div>
+
+          <form className="search-form">
+            <div className="form-group row">
+              <div className="col-md-9">
+                <input
+                  type="text"
+                  id="search-query"
+                  className="form-control"
+                  placeholder="Search for a five-day forecast"
+                />
+                
+              </div>
+              <div className="col-md-3">
+                <button type="button" className="btn btn-primary search">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+          <hr />
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3">
+                <h5>City</h5>
+              </div>
+
+              <div className="col-md-3">
+                <h5>Temperature (F)</h5>
+              </div>
+              <div className="col-md-3">
+                <h5>Pressure (hPa)</h5>
+              </div>
+              <div className="col-md-3">
+                <h5>Humidity (%)</h5>
+              </div>
+            </div>
+          </div>
+          <hr />
+
+
+
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">
+
+              </div>
+            </div>
+          </div>
+          <div className="fiveday row justify-content-center">
+
+          </div>
+        </div>
+      </div >
+    </div >
+
+
+  )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +22,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
